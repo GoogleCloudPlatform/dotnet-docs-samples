730b91778d8c160a5f479d3faeb88812fe6788e8
Jeffrey Rennie <rennie@google.com>
2015-11-03 10:21:08 -0800

    Fix the batch test script; fix GettingStarted build. Jenkins was reporting success even when some of the tests had failed, so add a little logic to the batch script to fix that. Then, fix the problem with the broken builds. And add TestResults/ to .gitignore for convenience.

--- buildAndRunTests.bat
+++ buildAndRunTests.bat
@@ -3,20 +3,28 @@ SET MSBUILD="%ProgramFiles(x86)%\MSBuild\14.0\Bin\MSBuild.exe"
 SET NUGET="%ProgramFiles(x86)%\MSBuild\14.0\Bin\Nuget.exe"
 SET MSTEST="%ProgramFiles(x86)%\Microsoft Visual Studio 14.0\Common7\IDE\MSTest.exe"
 REM Dump the environment variables.
+SET FAILED=0
 SET
 
 CD bigquery\api\BigQueryUtil
 %NUGET% restore
-%MSBUILD% 
-%MSTEST% /testcontainer:test\bin\debug\test.dll
+%MSBUILD% && %MSTEST% /testcontainer:test\bin\debug\test.dll || SET FAILED=1
 
 CD ..\GettingStarted
 %NUGET% restore
-%MSBUILD% 
-%MSTEST% /testcontainer:test\bin\debug\test.dll
+%MSBUILD% && %MSTEST% /testcontainer:test\bin\debug\test.dll || SET FAILED=1
 
 CD ..\..\..\storage\api
 %NUGET% restore
-%MSBUILD% 
-%MSTEST% /testcontainer:test\bin\debug\test.dll
+%MSBUILD% && %MSTEST% /testcontainer:test\bin\debug\test.dll || SET FAILED=1
+
+@ECHO OFF
+IF %FAILED% NEQ 0 GOTO failed_case
 ENDLOCAL
+ECHO SUCCEEDED
+EXIT /b 
+
+:failed_case
+ENDLOCAL
+ECHO FAILED
+EXIT /b 1
