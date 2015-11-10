0eeab355c42b7c342b5f6ef6107760b9b3d6e64e
Emily (remi) Taylor <remi@remitaylor.com>
2015-11-10 11:00:34 -0800

    Remove System import & update styling for inclusion in docs

--- pubsub/api/CreateTopic.cs
+++ pubsub/api/CreateTopic.cs
@@ -1,11 +1,9 @@
-﻿using System;
 using Google.Apis.Pubsub.v1;
 using Google.Apis.Pubsub.v1.Data;
 
-class CreateTopicSample
-{
-  public void CreateTopic(string projectId, string topicName)
-  {
+class CreateTopicSample {
+  
+  public void CreateTopic(string projectId, string topicName) {
     PubsubService PubSub = PubSubClient.Create();
 
     Topic topic = PubSub.Projects.Topics.Create(
@@ -13,6 +11,6 @@ class CreateTopicSample
       body: new Topic() { Name = topicName }
     ).Execute();
 
-    Console.WriteLine($"Created: {topic.Name}");
+    System.Console.WriteLine($"Created: {topic.Name}");
   }
 }
