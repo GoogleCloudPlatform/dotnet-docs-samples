8f952986db5a65476b1878ed5d64091e64b56d76
Jonathan Simon <jbsimon@ad.corp.google.com>
2015-12-04 14:49:17 -0800

    Adding Auth sample

---
+++ auth/Program.cs
@@ -0,0 +1,139 @@
+﻿/*
+ * Copyright (c) 2015 Google Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+// [START all]
+using Google.Apis.Auth.OAuth2;
+using Google.Apis.Storage.v1;
+using Google.Apis.Storage.v1.Data;
+using Google.Apis.Services;
+using System;
+using System.Threading.Tasks;
+
+namespace GoogleCloudSamples
+{
+    public class AuthSample
+    {
+        const string usage = @"Usage:AuthSample <bucket_name>";
+        // [START build_service]
+        /// <summary>
+        /// Creates an authorized Cloud Storage client service using Application 
+        /// Default Credentials.
+        /// </summary>
+        /// <returns>an authorized Cloud Storage client.</returns>
+        public static async Task<StorageService> CreateAuthorizedClientAsync()
+        {
+            GoogleCredential credential = 
+                await GoogleCredential.GetApplicationDefaultAsync();
+            // Inject the Cloud Storage scope if required.
+            if (credential.IsCreateScopedRequired)
+            {
+                credential = credential.CreateScoped(new[] 
+                {
+                    StorageService.Scope.DevstorageReadOnly
+                });
+            }
+            return new StorageService(new BaseClientService.Initializer()
+            {
+                HttpClientInitializer = credential,
+                ApplicationName = "DotNet Google Cloud Platform Auth Sample",
+            });
+        }
+        // [END build_service]
+
+        // [START list_storage_bucket_contents]
+        /// <summary>
+        /// List the contents of a Cloud Storage bucket.
+        /// </summary>
+        /// <param name="bucket">the name of the Cloud Storage bucket.</param>
+        ///<returns>a list of the contents of the specified bucket.</returns>
+        public static async Task<Objects> ExecuteApiRequestAsync(
+            StorageService storage, string bucket)
+        {
+            var request = new 
+                Google.Apis.Storage.v1.ObjectsResource.ListRequest(storage, 
+                bucket);
+            var requestResult = await request.ExecuteAsync();
+            return requestResult;
+        }
+        // [END list_storage_bucket_contents]
+
+        private static void Main(string[] args)
+        {
+            SamplesUtil.InvokeMain(() =>
+            {
+                var sample = new AuthSample();
+                sample.MainFunction(args);
+            });
+        }
+
+        void MainFunction(string[] args)
+        {
+            AuthSample sample = new AuthSample();
+            string bucket = null;
+            if (args.Length == 0)
+            {
+                Console.WriteLine(usage);
+                return;
+            }
+            bucket = args[0];
+            // Create a new Cloud Storage client authorized via Application 
+            // Default Credentials
+            StorageService storage = CreateAuthorizedClientAsync().Result;
+
+            try
+            {
+                // Use the Cloud Storage client to get a list of objects for the
+                // given bucket name
+                Objects result = ExecuteApiRequestAsync(storage, bucket).Result;
+
+                // Get enumerator to loop through list of objects
+                var resultsList = result.Items.GetEnumerator();
+
+                Console.WriteLine(
+                    "======= Listing Cloud Storage Bucket's contents =======");
+                Console.WriteLine();
+
+                // Loop through objects list, output object name and timestamp
+                while (resultsList.MoveNext())
+                {
+                    if (!resultsList.Current.Equals(null))
+                    {
+                        // Output object name and creation timestamp
+                        Console.WriteLine(resultsList.Current.Name.ToString());
+                        Console.WriteLine(
+                            resultsList.Current.TimeCreated.ToString());
+                        Console.WriteLine();
+                    } 
+                }
+            }
+            catch (Exception ex)
+            {
+                if (ex is AggregateException || ex is NullReferenceException)
+                {
+                    // No contents found for given bucket
+                    Console.WriteLine("No contents found for given bucket. " 
+                        + "Sign in to the Google Developers Console");
+                    Console.WriteLine(
+                        "at: https://console.developers.google.com/storage ");
+                    Console.WriteLine("to confirm your bucket name is valid " 
+                        + "and to upload some files to your bucket.");
+                }
+            }
+            Console.WriteLine("Press any key...");
+            Console.ReadKey();
+        }
+    }
+}
+// [END all]
---
+++ auth/README.md
@@ -0,0 +1,43 @@
+## Authentication Sample
+
+A sample demonstrating how to authenticate with Google's APIs using Application Default Credentials
+[Application Default Credentials](https://developers.google.com/identity/protocols/application-default-credentials)
+from C#. The sample authenticates and then makes an API request to the Compute Engine API.
+
+
+See our other [Google Cloud Platform github
+repos](https://github.com/GoogleCloudPlatform) for sample applications and
+scaffolding for other frameworks and use cases.
+
+## Build and Run
+0.  On your Windows development machine,
+1.  In the [Google Developers Console](https://console.developers.google.com/),
+    create a new project or choose an existing project.
+2.  In the [Google Developers Console](https://console.developers.google.com/),
+    click **APIs & auth**, then click APIs.  Wait for a list of APIs to
+    appear, then click Cloud Storage.  If Cloud Storage is not already enabled,
+    click the Enable API button.
+3.  In the [Google Developers Console](https://console.developers.google.com/),
+    under **APIs & auth**, click Credentials.  Click the button to "Generate
+    a new JSON key."  Set the environment variable
+    `GOOGLE_APPLICATION_CREDENTIALS` to the path of the JSON key you
+    downloaded.
+3.  Clone this repo with
+
+    ```sh
+    git clone https://github.com/GoogleCloudPlatform/csharp-docs-samples
+    ```
+4.  Open csharp-docs-samples/auth/OAuth2.sln with
+    Microsoft Visual Studio version 2012 or later.
+5.  Build the Solution.
+6.  Run it locally by pressing F5 or choosing "Debug -> Start Debugging" from
+    Microsoft Visual Studio's Menu.
+
+
+## Contributing changes
+
+* See [CONTRIBUTING.md](../../CONTRIBUTING.md)
+
+## Licensing
+
+* See [LICENSE](../../LICENSE)
