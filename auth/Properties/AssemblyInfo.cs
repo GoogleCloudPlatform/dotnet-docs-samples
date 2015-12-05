08a7172eb2a43d018627de9ad591a76c43b10a2c
Jonathan Simon <jbsimon@ad.corp.google.com>
2015-12-04 16:25:17 -0800

    Update Assembly Properties.

--- auth/Properties/AssemblyInfo.cs
+++ auth/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("BigQuery")]
+[assembly: AssemblyTitle("Auth")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("BigQuery")]
+[assembly: AssemblyProduct("Auth")]
 [assembly: AssemblyCopyright("Copyright \u00A9  2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
