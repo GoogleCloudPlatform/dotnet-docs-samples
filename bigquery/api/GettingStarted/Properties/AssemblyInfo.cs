7214ec9e2fa732bf9a1964bf048692e5dff17048
Jeffrey Rennie <rennie@google.com>
2016-04-08 17:10:17 -0700

    Run lint. Switch to powershell test script.

--- bigquery/api/GettingStarted/Properties/AssemblyInfo.cs
+++ bigquery/api/GettingStarted/Properties/AssemblyInfo.cs
@@ -9,7 +9,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("BigQuery")]
-[assembly: AssemblyCopyright("Copyright ©  2015")]
+[assembly: AssemblyCopyright("Copyright \u00A9  2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
