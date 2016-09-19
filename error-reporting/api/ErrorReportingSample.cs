05dce695ebf3e6d86d73748a388d779ab482ad8b
GOOGLE\talarico <talarico@google.com>
2016-09-19 13:55:31 -0700

    Add comments to the sample.

--- error-reporting/api/ErrorReportingSample.cs
+++ error-reporting/api/ErrorReportingSample.cs
@@ -47,10 +47,20 @@ namespace GoogleCloudSamples
             }
         }
 
+        /// <summary>
+        /// Create the Error Reporting service (<seealso cref="ClouderrorreportingService"/>)
+        /// with the Application Default Credentials and the proper scopes.
+        /// See: https://developers.google.com/identity/protocols/application-default-credentials.
+        /// </summary>
         private static ClouderrorreportingService CreateErrorReportingClient()
         {
+            // Get the Application Default Credentials.
             GoogleCredential credential = Task.Run(() => GoogleCredential.GetApplicationDefaultAsync()).Result;
+
+            // Add the needed scope to the credentials.
             credential.CreateScoped(ClouderrorreportingService.Scope.CloudPlatform);
+
+            // Create the Error Reporting Service.
             ClouderrorreportingService service = new ClouderrorreportingService(new BaseClientService.Initializer
             {
                 HttpClientInitializer = credential,
@@ -58,11 +68,23 @@ namespace GoogleCloudSamples
             return service;
         }
 
+        /// <summary>
+        /// Creates a <seealso cref="ReportRequest"/> from a given exception.
+        /// </summary>
         private static ReportRequest CreateReportRequest(Exception e)
         {
+            // Create the service.
             ClouderrorreportingService service = CreateErrorReportingClient();
+
+            // Get the project ID from the environement variables.
             string projectId = Environment.GetEnvironmentVariable("GOOGLE_PROJECT_ID");
-            string formattedProjectId = string.Format("projects/{0}", "error-reporting-c-sharp-test");
+
+            // Format the project id to the format Error Reporting expects. See:
+            // https://cloud.google.com/error-reporting/reference/rest/v1beta1/projects.events/report
+            string formattedProjectId = string.Format("projects/{0}", projectId);
+
+            // Add a service context to the report.  For more details see:
+            // https://cloud.google.com/error-reporting/reference/rest/v1beta1/projects.events#ServiceContext
             ServiceContext serviceContext = new ServiceContext()
             {
                 Service = "myapp",
@@ -76,8 +98,12 @@ namespace GoogleCloudSamples
             return new ReportRequest(service, errorEvent, formattedProjectId);
         }
 
+        /// <summary>
+        /// Report an exception to the Error Reporting service.
+        /// </summary>
         private static void report(Exception e)
         {
+            // Create the report and execute the request.
             ReportRequest request = CreateReportRequest(e);
             request.Execute();
         }
