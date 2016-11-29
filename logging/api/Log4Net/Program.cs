34280987a5b0a483ccb1ec76a7e0b6e440e6d42b
Jonathan Simon <jbsimon@google.com>
2016-11-29 07:20:15 -0800

    Add licenses

--- logging/api/Log4Net/Program.cs
+++ logging/api/Log4Net/Program.cs
@@ -1,3 +1,19 @@
+/*
+ * Copyright (c) 2016 Google Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
 ﻿using System;
 using System.IO;
 using log4net;
@@ -16,10 +32,13 @@ namespace GoogleCloudSamples
             {
                 configFile = Path.Combine(args[0], configFile);
             }
-            // [START log4net_log_entry]
+            // [START log4net_config]
             // Configure log4net to use Stackdriver logging from the XML configuration file.
             XmlConfigurator.Configure(new FileInfo(configFile));
+            // [END log4net_config]
 
+
+            // [START log4net_log_entry]
             // Retrieve a logger for this context.
             ILog log = LogManager.GetLogger(typeof(Program));
 
