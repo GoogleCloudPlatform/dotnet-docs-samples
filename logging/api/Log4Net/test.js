a5167e6285bc85ef8a4fd6bd6893714f7c838f6d
Jonathan Simon <jbsimon@google.com>
2016-12-02 16:37:09 -0800

    Log4Net sample ASP.NET app

---
+++ logging/api/Log4Net/test.js
@@ -0,0 +1,26 @@
+// Copyright(c) 2015 Google Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License"); you may not
+// use this file except in compliance with the License. You may obtain a copy of
+// the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+// WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+// License for the specific language governing permissions and limitations under
+// the License.
+
+// 1.0 style test script not using the `casperjs test` subcommand
+var casper = require('casper').create();
+var host = casper.cli.args[0];
+
+casper.start(host + '/', function (response) {
+    this.test.assertEquals(200, response.status);
+});
+
+casper.run(function () {
+    this.test.done(1);
+    this.test.renderResults(true);
+});
