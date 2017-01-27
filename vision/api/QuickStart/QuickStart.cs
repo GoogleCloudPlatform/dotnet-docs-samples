2d8831da460ebb032c4931266b051ea968819001
Jeffrey Rennie <rennie@google.com>
2017-01-26 16:43:43 -0800

    Add comments to vision quickstart.

--- vision/api/QuickStart/QuickStart.cs
+++ vision/api/QuickStart/QuickStart.cs
@@ -23,8 +23,11 @@ namespace GoogleCloudSamples
     {
         public static void Main(string[] args)
         {
+            // Instantiates a client
             var client = ImageAnnotatorClient.Create();
+            // Load the image file into memory
             var image = Image.FromFile("wakeupcat.jpg");
+            // Performs label detection on the image file
             var response = client.DetectLabels(image);
             foreach (var annotation in response)
             {
