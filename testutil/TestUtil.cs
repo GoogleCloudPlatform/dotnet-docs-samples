9a927170a0170ae7c365add024a037b43b9dbbb3
Jeffrey Rennie <rennie@google.com>
2017-02-24 14:42:46 -0800

    Move RandomBucketName() to RandomName().

--- testutil/TestUtil.cs
+++ testutil/TestUtil.cs
@@ -18,11 +18,32 @@ using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Threading;
 using Xunit;
 
 namespace GoogleCloudSamples
 {
+    public class TestUtil
+    {
+        public static string RandomName()
+        {
+            using (RandomNumberGenerator rng = new RNGCryptoServiceProvider())
+            {
+                string legalChars = "abcdefhijklmnpqrstuvwxyz";
+                byte[] randomByte = new byte[1];
+                var randomChars = new char[20];
+                int nextChar = 0;
+                while (nextChar < randomChars.Length)
+                {
+                    rng.GetBytes(randomByte);
+                    if (legalChars.Contains((char)randomByte[0]))
+                        randomChars[nextChar++] = (char)randomByte[0];
+                }
+                return new string(randomChars);
+            }
+        }
+    }
     public class RetryRobot
     {
         public int FirstRetryDelayMs { get; set; } = 1000;
