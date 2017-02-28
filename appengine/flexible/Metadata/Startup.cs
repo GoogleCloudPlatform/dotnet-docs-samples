8a288a962bde0c7d8b7d1318387067e2a1ed003e
Jeffrey Rennie <rennie@google.com>
2017-02-28 11:20:15 -0800

    Appengine metadata sample.

---
+++ appengine/flexible/Metadata/Startup.cs
@@ -0,0 +1,65 @@
+﻿/*
+ * Copyright (c) 2017 Google Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+namespace Metadata
+{
+    public class Startup
+    {
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        {
+            loggerFactory.AddConsole();
+
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            app.Run(async (context) =>
+            {
+                string text;
+                var metadataClient = Google.Cloud.Metadata.V1.MetadataClient.Create();
+                try
+                {
+                    var result = await metadataClient.GetMetadataAsync(
+                        "instance/network-interfaces/0/access-configs/0/external-ip");
+                    text = string.Format(@"<html><head><title>Metadata Succeeded</title></head>
+                    <body>My public IP address is {0}</body></html>",
+                        result.Content.ToString());
+                }
+                catch (System.Net.Http.HttpRequestException)
+                {
+                    // Must not be running on App Engine.
+                    text = @"<html><head><title>Metadata Error</title></head>
+                    <body>I am not running in the Google Cloud.
+                    </body></html>";
+                }
+                context.Response.ContentType = "text/html";
+                await context.Response.WriteAsync(text);
+            });
+        }
+    }
+}
