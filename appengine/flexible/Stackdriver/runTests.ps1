11ef10c7c5f535b2b866a3dcf79c070118002369
Jonathan Simon <jbsimon@google.com>
2017-05-31 14:13:31 -0700

    Capitalize projectId placeholder.

--- appengine/flexible/Stackdriver/runTests.ps1
+++ appengine/flexible/Stackdriver/runTests.ps1
@@ -16,7 +16,7 @@ Import-Module -DisableNameChecking ..\..\..\BuildTools.psm1
 
 dotnet restore
 BackupAndEdit-TextFile "appsettings.json" `
-    @{"your-google-project-id" = $env:GOOGLE_PROJECT_ID} `
+    @{"YOUR-GOOGLE-PROJECT-ID" = $env:GOOGLE_PROJECT_ID} `
 {
 	dotnet build
 	Run-KestrelTest 5582
