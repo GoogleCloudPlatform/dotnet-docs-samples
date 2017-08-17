819b59294eae7c740167a15990d5f82329b9b397
Jeffrey Rennie <rennie@google.com>
2017-08-16 17:00:31 -0700

    Making spanner tests more resilient.

---
+++ spanner/api/Create-SpannerDatabase.ps1
@@ -0,0 +1,5 @@
+﻿$configListText = gcloud spanner instance-configs list
+$configList = $configListText -replace "\s+", "," | ConvertFrom-Csv
+$usCentral = $configList | Where-Object { $_.NAME -like "*us-central*"} | Select -ExpandProperty Name | Select -First 1
+gcloud spanner instances create my-instance --config=$usCentral --description="IntegrationTest" --nodes=1
+gcloud spanner databases create my-database --instance=my-instance
