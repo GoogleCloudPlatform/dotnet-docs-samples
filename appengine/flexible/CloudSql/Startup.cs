c348cff814b20ee57a629b54ced5bfc8afac4659
Jeffrey Rennie <rennie@google.com>
2017-09-21 11:53:30 -0700

    WIP. Error handling.

--- appengine/flexible/CloudSql/Startup.cs
+++ appengine/flexible/CloudSql/Startup.cs
@@ -17,6 +17,7 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc.Filters;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
@@ -44,8 +45,10 @@ namespace CloudSql
         public IConfigurationRoot Configuration { get; }
         IServiceCollection _services;
 
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
+        // This method gets called by the runtime. Use this method to add
+        // services to the container.
+        // For more information on how to configure your application, visit
+        // http://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddSingleton(typeof(DbConnection), (IServiceProvider) => {
@@ -56,6 +59,9 @@ namespace CloudSql
                 // [END connection]
                 CreateTable(connection);
                 return connection;
+            });
+            services.AddMvc(options => {
+                options.Filters.Add(typeof(DbExceptionFilterAttribute));
             });
             _services = services;
         }
@@ -75,7 +81,8 @@ namespace CloudSql
 
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env,
+            ILoggerFactory loggerFactory)
         {
             loggerFactory.AddConsole(Configuration.GetSection("Logging"));
 
@@ -96,80 +103,5 @@ namespace CloudSql
             });
         }
 
-        public void OldConfigure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
-        {
-            loggerFactory.AddConsole();
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
-            DbConnection connection;
-
-                app.Run(async (context) =>
-                {
-                    await context.Response.WriteAsync(string.Format(@"<html>
-                        <head><title>Error</title></head>
-                        <body><p>Set CloudSqlConnectionString to a valid connection string.
-                              <p>{0}
-                              p>See the README.md in the project directory for more information.</p>
-                        </body>
-                        </html>", WebUtility.HtmlEncode(e.Message)));
-                });
-                return;
-        
-
-            app.Run(async (HttpContext context) =>
-            {
-                // [START example]
-                // Insert a visit into the database:
-                using (var insertVisitCommand = connection.CreateCommand())
-                {
-                    insertVisitCommand.CommandText =
-                        @"INSERT INTO visits (user_ip) values (@user_ip)";
-                    var ip = insertVisitCommand.CreateParameter();
-                    ip.DbType = DbType.String;
-                    ip.Value =
-                        FormatAddress(context.Connection.RemoteIpAddress);
-                    insertVisitCommand.Parameters["@user_ip"] = ip;
-                    await insertVisitCommand.ExecuteNonQueryAsync();
-                }
-
-                // Look up the last 10 visits.
-                using (var lookupCommand = connection.CreateCommand())
-                {
-                    lookupCommand.CommandText = @"
-                        SELECT * FROM visits
-                        ORDER BY time_stamp DESC LIMIT 10";
-                    List<string> lines = new List<string>();
-                    var reader = await lookupCommand.ExecuteReaderAsync();
-                    while (await reader.ReadAsync())
-                        lines.Add($"{reader.GetString(0)} {reader.GetString(1)}");
-                    await context.Response.WriteAsync(string.Format(@"<html>
-                        <head><title>Visitor Log</title></head>
-                        <body>Last 10 visits:<br>{0}</body>
-                        </html>", string.Join("<br>", lines)));
-                }
-                // [END example]
-            });
-        }
-
-        private string FormatAddress(IPAddress address)
-        {
-            if (address.AddressFamily == System.Net.Sockets.AddressFamily.InterNetworkV6)
-            {
-                var bytes = address.GetAddressBytes();
-                return string.Format("{0:X2}{1:X2}:{2:X2}{3:X2}", bytes[0], bytes[1],
-                    bytes[2], bytes[3]);
-            }
-            else if (address.AddressFamily == System.Net.Sockets.AddressFamily.InterNetworkV6)
-            {
-                var bytes = address.GetAddressBytes();
-                return string.Format("{0}.{1}", bytes[0], bytes[1]);
-            }
-            else
-            {
-                return "bad.address";
-            }
-        }
     }
 }
