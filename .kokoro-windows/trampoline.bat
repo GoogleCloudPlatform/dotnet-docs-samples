3f2bd2f1b7069e95117c2aaa2665d955c68bca5e
Jeffrey Rennie <rennie@google.com>
2017-10-19 12:42:16 -0700

    Kokoro windows (#360)
    * Copy .kokoro to .kokoro-windows.
    
    * Just dump current directory.
    
    * Fix paths to trampoline build file.
    
    * Port trampoline.sh to trampoline.bat.
    
    * Full path to python.exe.
    
    * Add some debugging lines.
    
    * Try to run some real tests.
    
    * Remove unused system_tests.sh.

---
+++ .kokoro-windows/trampoline.bat
@@ -0,0 +1,15 @@
+REM Copyright 2017 Google Inc.
+REM
+REM Licensed under the Apache License, Version 2.0 (the "License");
+REM you may not use this file except in compliance with the License.
+REM You may obtain a copy of the License at
+REM
+REM      http://www.apache.org/licenses/LICENSE-2.0
+REM
+REM Unless required by applicable law or agreed to in writing, software
+REM distributed under the License is distributed on an "AS IS" BASIS,
+REM WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+REM See the License for the specific language governing permissions and
+REM limitations under the License.
+choco install -y dotnetcore-sdk
+powershell github\dotnet-docs-samples\.kokoro-windows\main.ps1
