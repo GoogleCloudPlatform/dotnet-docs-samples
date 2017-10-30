faac832e726eaa843623fd76e6b75936893e938c
Jonathan Simon <jbsimon@ad.corp.google.com>
2017-10-30 13:15:08 -0700

    Update Video sample to .NET Core 2.0

--- .kokoro/main.ps1
+++ .kokoro/main.ps1
@@ -25,7 +25,8 @@ try {
     Import-Module  .\BuildTools.psm1 -DisableNameChecking
 
     # The list of directories with runTests that have been ported to dotnet core.
-    $dirs = @('appengine', 'auth', 'datastore', 'kms', 'language', 'monitoring', 'pubsub', 'speech', 'translate')
+    $dirs = @('appengine', 'auth', 'datastore', 'kms', 'language', 'monitoring', 
+      'pubsub', 'speech', 'translate', 'video')
 
     # Find all the runTest scripts.
     $scripts = Get-ChildItem -Path $dirs -Filter *runTest*.ps* -Recurse
