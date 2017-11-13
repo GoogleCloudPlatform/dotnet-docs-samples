967f3735eedcdb010a79cd144282e5748817f7a3
Jeffrey Rennie <rennie@google.com>
2017-11-13 12:56:45 -0800

    Stop warning about verbs.

--- video/api/Test/runTests.ps1
+++ video/api/Test/runTests.ps1
@@ -11,7 +11,7 @@
 # WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
 # License for the specific language governing permissions and limitations under
 # the License.
-import-module ..\..\..\BuildTools.psm1
+import-module -DisableNameChecking ..\..\..\BuildTools.psm1
 
 Set-TestTimeout 900
 
