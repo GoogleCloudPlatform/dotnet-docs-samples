09eeb8f0fd6bd5db4c91a3e93f4b9ffd6773cac5
Jeffrey Rennie <rennie@google.com>
2018-01-19 13:18:59 -0800

    Extend datastore test timeout because it often takes longer.

--- datastore/api/TaskListTest/runTests.ps1
+++ datastore/api/TaskListTest/runTests.ps1
@@ -11,6 +11,8 @@
 # WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
 # License for the specific language governing permissions and limitations under
 # the License.
+Import-Module -DisableNameChecking ..\..\..\BuildTools.psm1
 
+Set-TestTimeout 900
 dotnet restore
 dotnet test -v n
