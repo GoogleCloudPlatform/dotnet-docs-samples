84446e1c9c7bc723dd42b6d92ccdcf84d73c7e8a
jsimonweb <jsimonweb@gmail.com>
2018-03-29 10:20:43 -0700

    Update Casper version for RedisCache sample test.

--- appengine/flexible/RedisCache/test.js
+++ appengine/flexible/RedisCache/test.js
@@ -12,47 +12,47 @@
 // License for the specific language governing permissions and limitations under
 // the License.
 
-// 1.0 style test script not using the `casperjs test` subcommand
-var casper = require('casper').create();
-var host = casper.cli.args[0];
+var system = require('system');
+var host = system.env['CASPERJS11_URL'];
 
 currentCount = 0;
 
-casper.start(host + '/', function (response) {
-    console.log('Starting ' + host + '/');
-    this.test.assertSelectorHasText('H1', 'Redis Incrementer Sample');
-    currentCount = parseInt(this.fetchText("#CurrentCount"), 10);
-    console.log("Current count is " + currentCount);
-    this.fill('#WhoForm', {
-        'Who': 'test.js',
-    }, false);
-    console.log("Filled form.");
-});
+casper.test.begin('Test RedisCache sample.', 3, function suite(test) {
+    casper.start(host + '/', function (response) {
+        console.log('Starting ' + host + '/');
+        test.assertSelectorHasText('H1', 'Redis Incrementer Sample');
+        currentCount = parseInt(this.fetchText("#CurrentCount"), 10);
+        console.log("Current count is " + currentCount);
+        this.fill('#WhoForm', {
+            'Who': 'test.js',
+        }, false);
+        console.log("Filled form.");
+    });
 
-casper.thenClick('#Submit', function (response) {
-    console.log('Submitted form.');
-    this.test.assertEquals(200, response.status);
-    this.test.assertSelectorHasText('#LastIncrementedBy', 'test.js');
-    this.test.assertSelectorHasText('#CurrentCount', currentCount + 1);
-    this.fill('#WhoForm', {
-        'Who': 'anothertest.js',
-    }, false);
-    console.log("Filled another form.");
-});
+    casper.thenClick('#Submit', function (response) {
+        console.log('Submitted form.');
+        test.assertEquals(200, response.status);
+        test.assertSelectorHasText('#LastIncrementedBy', 'test.js');
+        test.assertSelectorHasText('#CurrentCount', currentCount + 1);
+        this.fill('#WhoForm', {
+            'Who': 'anothertest.js',
+        }, false);
+        console.log("Filled another form.");
+    });
 
-casper.thenClick('#Submit', function (response) {
-    console.log('Submitted form.');
-    this.test.assertEquals(200, response.status);
-    this.test.assertSelectorHasText('#LastIncrementedBy', 'anothertest.js');
-});
+    casper.thenClick('#Submit', function (response) {
+        console.log('Submitted form.');
+        test.assertEquals(200, response.status);
+        test.assertSelectorHasText('#LastIncrementedBy', 'anothertest.js');
+    });
 
-casper.thenClick('#Reset', function (response) {
-    console.log('Clicked Reset.');
-    this.test.assertEquals(200, response.status);
-    this.test.assertSelectorHasText('#CurrentCount', '0');
-});
+    casper.thenClick('#Reset', function (response) {
+        console.log('Clicked Reset.');
+        test.assertEquals(200, response.status);
+        test.assertSelectorHasText('#CurrentCount', '0');
+    });
 
-casper.run(function () {
-    this.test.done();
-    this.test.renderResults(true);
+    casper.run(function () {
+        test.done();
+    });
 });
