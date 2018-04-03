8f8c5a735a409edd316e062c4927cd114b43f378
Mete Atamel <atamel@google.com>
2018-04-03 15:06:07 -0300

    Updated NuGet dependencies to latest

--- applications/googlehome-meets-dotnetcontainers/GoogleHomeAspNetCoreDemoServer/Dialogflow/Intents/Vision/VisionSafeHandler.cs
+++ applications/googlehome-meets-dotnetcontainers/GoogleHomeAspNetCoreDemoServer/Dialogflow/Intents/Vision/VisionSafeHandler.cs
@@ -11,7 +11,7 @@
 // WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
 // License for the specific language governing permissions and limitations under
 // the License.
-using Google.Cloud.Vision.V1P1Beta1;
+using Google.Cloud.Vision.V1;
 using System.Linq;
 using System.Threading.Tasks;
 
