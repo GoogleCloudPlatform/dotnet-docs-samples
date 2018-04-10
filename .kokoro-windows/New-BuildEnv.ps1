884a3bccfaafe478552f398d988fdc25a6f1a13c
Jeffrey Rennie <rennie@google.com>
2018-04-10 09:55:50 -0700

    Revert unrelated changes.

--- .kokoro-windows/New-BuildEnv.ps1
+++ .kokoro-windows/New-BuildEnv.ps1
@@ -14,40 +14,37 @@
 Param ($Dir, [switch]$SkipDownloadKokoroDir)
 
 if (-Not $Dir) {
-    $Dir = "$PSScriptRoot/../env"
+    $Dir = "$PSScriptRoot\..\env"
 }
 
-if ([environment]::OSVersion.Platform -match 'Win*') {
-    # Install choco packages.
-    get-command choco -ErrorAction Stop
-    $chocoPackages = (choco list -li) -join ' '
+# Install choco packages.
+get-command choco -ErrorAction Stop
+$chocoPackages = (choco list -li) -join ' '
 
-    if (-not $chocoPackages.Contains('Microsoft .NET Core SDK - 2.0.')) {
-        choco install -y --sxs dotnetcore-sdk --version 2.0.0    
-    }
-
-    if (-not $chocoPackages.Contains('.NET Core SDK 1.1.')) {
-        choco install -y --sxs dotnetcore-sdk --version 1.1.2    
-    }
-
-    if (-not $chocoPackages.Contains('nuget.commandline 4.5.')) {
-        choco install -y nuget.commandline
-    }   
-
-    if (-not $chocoPackages.Contains('microsoft-build-tools 14.')) {
-        choco install -y --sxs microsoft-build-tools --version 14.0.23107.10
-    }
-
-    if (-not (($chocoPackages.Contains('python 2.7.') -or
-        (Test-Path "$env:SystemDrive/Python27")))) {
-        choco install -y --sxs python --version 2.7.6
-    }
-
-    if (-not $chocoPackages.Contains('selenium-chrome-driver 2.')) {
-        choco install -y selenium-chrome-driver
-    }
+if (-not $chocoPackages.Contains('Microsoft .NET Core SDK - 2.0.')) {
+    choco install -y --sxs dotnetcore-sdk --version 2.0.0    
 }
 
+if (-not $chocoPackages.Contains('.NET Core SDK 1.1.')) {
+    choco install -y --sxs dotnetcore-sdk --version 1.1.2    
+}
+
+if (-not $chocoPackages.Contains('nuget.commandline 4.5.')) {
+    choco install -y nuget.commandline
+}   
+
+if (-not $chocoPackages.Contains('microsoft-build-tools 14.')) {
+    choco install -y --sxs microsoft-build-tools --version 14.0.23107.10
+}
+
+if (-not (($chocoPackages.Contains('python 2.7.') -or
+    (Test-Path "$env:SystemDrive\Python27")))) {
+    choco install -y --sxs python --version 2.7.6
+}
+
+if (-not $chocoPackages.Contains('selenium-chrome-driver 2.')) {
+    choco install -y selenium-chrome-driver
+}
 # Create environment directory structure.
 $Dir = (New-Item -Path $Dir -ItemType Directory -Force).FullName
 $installDir = Join-Path $Dir 'install'
@@ -68,25 +65,25 @@ function Unzip([string]$zipfile, [string]$outpath)
 
 Set-PsDebug -Trace 1
 # Install codeformatter
-Unzip $env:KOKORO_GFILE_DIR/codeformatter.zip $installDir/codeformatter
+Unzip $env:KOKORO_GFILE_DIR\codeformatter.zip $installDir\codeformatter
 # Install phantomjs
-Unzip $env:KOKORO_GFILE_DIR/phantomjs-1.9.8-windows.zip $installDir
+Unzip $env:KOKORO_GFILE_DIR\phantomjs-2.1.1-windows.zip $installDir
 # Install casperjs
-Unzip $env:KOKORO_GFILE_DIR/n1k0-casperjs-1.0.3-0-g76fc831.zip $installDir
+Unzip $env:KOKORO_GFILE_DIR\n1k0-casperjs-1.0.3-0-g76fc831.zip $installDir
 # Patch casperjs
-Copy-Item -Force $PSScriptRoot/../.kokoro/docker/bootstrap.js `
-    $installDir/n1k0-casperjs-76fc831/bin/bootstrap.js
+Copy-Item -Force $PSScriptRoot\..\.kokoro\docker\bootstrap.js `
+    $installDir\n1k0-casperjs-76fc831\bin\bootstrap.js
 # Install casperjs 1.1
-Unzip $env:KOKORO_GFILE_DIR/casperjs-1.1.4-1.zip $installDir
+Unzip $env:KOKORO_GFILE_DIR\casperjs-1.1.4-1.zip $installDir
 Set-PsDebug -Off
 
 # Copy Activate.ps1 to the environment directory.
 # And append 3 more lines.
-$activatePs1 = Get-Content $PSScriptRoot/Activate.ps1
-$importSecretsPath = "$PSScriptRoot/Import-Secrets.ps1"
-$buildToolsPath = Resolve-Path "$PSScriptRoot/../BuildTools.psm1"
+$activatePs1 = Get-Content $PSScriptRoot\Activate.ps1
+$importSecretsPath = "$PSScriptRoot\Import-Secrets.ps1"
+$buildToolsPath = Resolve-Path "$PSScriptRoot\..\BuildTools.psm1"
 @($activatePs1, '', 
     "`$env:KOKORO_GFILE_DIR = '$env:KOKORO_GFILE_DIR'",
     "& '$importSecretsPath'",
     "Import-Module -DisableNameChecking '$buildToolsPath'") `
-    | Out-File -Force $Dir/Activate.ps1
+    | Out-File -Force $Dir\Activate.ps1
