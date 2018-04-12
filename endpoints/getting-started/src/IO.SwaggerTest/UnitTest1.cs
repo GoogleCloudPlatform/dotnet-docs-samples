c158fd761eb1fbb031fa69b0c857ecd4f70da6f6
Jeffrey Rennie <rennie@google.com>
2018-04-12 16:52:03 -0700

    Lint

--- endpoints/getting-started/src/IO.SwaggerTest/UnitTest1.cs
+++ endpoints/getting-started/src/IO.SwaggerTest/UnitTest1.cs
@@ -1,3 +1,17 @@
+﻿// Copyright (c) 2018 Google LLC.
+// 
+// Licensed under the Apache License, Version 2.0 (the "License"); you may not
+// use this file except in compliance with the License. You may obtain a copy of
+// the License at
+// 
+// http://www.apache.org/licenses/LICENSE-2.0
+// 
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+// WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+// License for the specific language governing permissions and limitations under
+// the License.
+
 using System;
 using System.Collections.Generic;
 using System.Net;
@@ -10,7 +24,7 @@ namespace IO.SwaggerTest
 {
     public class UnitTest1
     {
-        class Payload 
+        class Payload
         {
             public string Message { get; set; }
         }
@@ -20,17 +34,17 @@ namespace IO.SwaggerTest
         {
             var request = HttpWebRequest.Create("http://localhost:7412/echo");
             request.Method = "POST";
-            request.ContentType= "application/json";
+            request.ContentType = "application/json";
             using (var streamWriter = new StreamWriter(request.GetRequestStream()))
             {
                 streamWriter.Write(JsonConvert.SerializeObject(
                     new Payload { Message = "in a bottle" }));
             }
-            var response = (HttpWebResponse) await request.GetResponseAsync();
+            var response = (HttpWebResponse)await request.GetResponseAsync();
             Assert.Equal(HttpStatusCode.OK, response.StatusCode);
             var responseString = await new StreamReader(
                 response.GetResponseStream()).ReadToEndAsync();
-            var payload = JsonConvert.DeserializeObject<Payload>(responseString);       
+            var payload = JsonConvert.DeserializeObject<Payload>(responseString);
             Assert.Equal("in a bottle", payload.Message);
         }
     }
