415d40a7628a2ae92dc8f38fdc118a3cb76fdaab
Jeffrey Rennie <rennie@google.com>
2018-04-19 12:39:50 -0700

    Complete Initialize-GoogleProjectForTesting.

--- .kokoro-windows/Initialize-GoogleProjectForTesting.ps1
+++ .kokoro-windows/Initialize-GoogleProjectForTesting.ps1
@@ -12,20 +12,69 @@
 # License for the specific language governing permissions and limitations under
 # the License.
 
+##############################
+#.SYNOPSIS
+# Enables all the necessary APIs and binds user to roles to be able to run
+# kokoro tests.
+#
+#
+#.PARAMETER serviceAccountEmail
+#The email address of the service account used to run tests.
+#
+#.PARAMETER projectId
+#The google cloud project id where the tests will run.
+##############################
 param([string][Parameter(Mandatory=$true)]$serviceAccountEmail, 
     [string][Parameter(Mandatory=$true)]$projectId)
 
-$services = @(
-    'dlp.googleapis.com', 
-    'clouderrorreporting.googleapis.com')
-$roles = @(
-    'roles/dlp.admin',
-    'roles/errorreporting.admin',
-    'roles/cloudkms.admin',
-    'roles/cloudkms.cryptoKeyEncrypterDecrypter',
-    'roles/logging.logWriter',
-    'roles/logging.admin',
-    'roles/logging.privateLogViewer')
+$services = @"
+bigquery-json.googleapis.com
+clouddebugger.googleapis.com
+datastore.googleapis.com
+storage-component.googleapis.com
+pubsub.googleapis.com
+speech.googleapis.com
+vision.googleapis.com
+storage-api.googleapis.com
+stackdriver.googleapis.com
+clouderrorreporting.googleapis.com
+logging.googleapis.com
+cloudapis.googleapis.com
+monitoring.googleapis.com
+language.googleapis.com
+stackdriverprovisioning.googleapis.com
+compute.googleapis.com
+sql-component.googleapis.com
+cloudkms.googleapis.com
+cloudtrace.googleapis.com
+dlp.googleapis.com
+servicemanagement.googleapis.com
+videointelligence.googleapis.com
+translate.googleapis.com
+"@
+
+$roles = @"
+roles/bigquery.admin
+roles/clouddebugger.user
+roles/cloudkms.admin
+roles/cloudkms.cryptoKeyEncrypterDecrypter
+roles/cloudsql.client
+roles/cloudsql.editor
+roles/cloudtrace.user
+roles/datastore.user
+roles/dlp.admin
+roles/dlp.user
+roles/errorreporting.admin
+roles/errorreporting.user
+roles/iap.httpsResourceAccessor
+roles/logging.admin
+roles/logging.logWriter
+roles/logging.privateLogViewer
+roles/logging.viewer
+roles/monitoring.admin
+roles/pubsub.admin
+roles/storage.admin
+"@
 
 Write-Host "Enabling $services..."
 gcloud services enable $services
@@ -37,8 +86,10 @@ function Bind($serviceAccountEmail, $role, $projectId) {
     }
 }
 
-foreach ($role in $roles) {
-    Bind $serviceAccountEmail $role $projectId
+foreach ($role in $roles.Split()) {
+    if ($role) {
+        Bind $serviceAccountEmail $role $projectId
+    }
 }
 
 # Special binding for IAP.
