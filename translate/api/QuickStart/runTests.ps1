fe0cd29fcf9227a3c9296f11b7671451650ee6ee
Jeffrey Rennie <rennie@google.com>
2018-09-14 09:10:23 -0700

    Replace translate/api/Translate.csproj with new canonical samples.

--- translate/api/QuickStart/runTests.ps1
+++ translate/api/QuickStart/runTests.ps1
@@ -1,4 +1,4 @@
-# Copyright(c) 2017 Google Inc.
+# Copyright(c) 2018 Google Inc.
 #
 # Licensed under the Apache License, Version 2.0 (the "License"); you may not
 # use this file except in compliance with the License. You may obtain a copy of
@@ -12,5 +12,4 @@
 # License for the specific language governing permissions and limitations under
 # the License.
 
-dotnet restore
-dotnet test --test-adapter-path:. --logger:junit
+dotnet run
