5477ab7979b073d8dee5aad75c8413b5451fbf49
Robin Reynolds-Haertle <RobinRH@users.noreply.github.com>
2018-12-12 16:28:10 -0800

    Add snippet tags to code sample that deletes data.
    I want to include this example in the docs.

--- spanner/api/Spanner/Program.cs
+++ spanner/api/Spanner/Program.cs
@@ -1536,6 +1536,7 @@ namespace GoogleCloudSamples.Spanner
         }
         // [END spanner_dml_partitioned_delete]
 
+        // [START spanner_delete_data]	    
         public static async Task DeleteSampleDataAsync(
             string projectId, string instanceId, string databaseId)
         {
@@ -1561,7 +1562,7 @@ namespace GoogleCloudSamples.Spanner
             {
                 await connection.OpenAsync();
 
-                // Insert rows into the Singers table.
+                // Delete rows from the Singers table.
                 var cmd = connection.CreateDeleteCommand("Singers",
                     new SpannerParameterCollection {
                         {"SingerId", SpannerDbType.Int64}
@@ -1575,6 +1576,8 @@ namespace GoogleCloudSamples.Spanner
                 Console.WriteLine("Deleted data.");
             }
         }
+        // [END spanner_delete_data]
+
         // [START spanner_insert_data]
         public static async Task InsertSampleDataAsync(
             string projectId, string instanceId, string databaseId)
