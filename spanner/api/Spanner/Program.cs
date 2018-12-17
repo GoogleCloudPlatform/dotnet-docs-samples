26bc59f62372fb9213fe3ac33d632224a55d70a4
Robin Reynolds-Haertle <RobinRH@users.noreply.github.com>
2018-12-17 13:19:34 -0800

    Update Program.cs

--- spanner/api/Spanner/Program.cs
+++ spanner/api/Spanner/Program.cs
@@ -2268,7 +2268,7 @@ namespace GoogleCloudSamples.Spanner
                     SingerId       INT64 NOT NULL,
                     VenueId        INT64 NOT NULL,
                     EventDate      Date,
-                    Revenue        INT64
+                    Revenue        INT64,
                     LastUpdateTime TIMESTAMP NOT NULL OPTIONS (allow_commit_timestamp=true)
                 ) PRIMARY KEY (SingerId, VenueId, EventDate),
                     INTERLEAVE IN PARENT Singers ON DELETE CASCADE";
