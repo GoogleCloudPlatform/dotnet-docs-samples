445eeb0b7f82ede863aa044f941cfd590673605f
Jeffrey Rennie <rennie@google.com>
2019-02-05 11:42:45 -0800

    CreateIncident() is code complete.
    Change-Id: I480f363bf63a3a70a66da0430369fe0e470d144f

--- irm/api/Snippets/CreateIncident.cs
+++ irm/api/Snippets/CreateIncident.cs
@@ -1,9 +1,35 @@
-using static Google.Cloud.Irm.V1Alpha2.IncidentService;
+using Google.Api.Gax.ResourceNames;
+using Google.Cloud.Irm.V1Alpha2;
+using Google.Protobuf.WellKnownTypes;
+using static Google.Cloud.Irm.V1Alpha2.Incident.Types;
+using System;
 
 class IrmCreateIncident {
-    public void CreateIncident()
+    public void CreateIncident(string projectId = "YOUR-PROJECT-ID")
     {
-        // No create method!
-        IncidentServiceClient incidents = IncidentServiceClient.Create();
+        // Create client
+        IncidentServiceClient incidentServiceClient = IncidentServiceClient.Create();
+        // Describe the incident.
+        Incident incident = new Incident()
+        {
+            Title = "Somebody pushed the red button!",
+            Synopsis = new Synopsis() 
+            {
+                Author = new User() 
+                {
+                    Email = "janedoe@example.com",                    
+                },
+                Content = "Nobody should ever push the red button.",
+                ContentType = "text/plain",
+                UpdateTime = DateTime.UtcNow.ToTimestamp()
+            },
+            StartTime = DateTime.UtcNow.ToTimestamp(),
+            Severity = Severity.Major,
+            Stage = Stage.Unspecified
+        };
+        string parent = new ProjectName(projectId).ToString();
+        // Call the API to create the incident.
+        Incident response = incidentServiceClient.CreateIncident(incident, parent);
+        Console.WriteLine("Created incident {0}.", response.Name);
     }
 }
--- irm/api/Snippets/Test.cs
+++ irm/api/Snippets/Test.cs
@@ -1,14 +1,11 @@
 using System;
 using Xunit;
 
-namespace Irm
+public class Test
 {
-    public class UnitTest1
-    {
-        [Fact]
-        public void Test1()
-        {
-
-        }
+    [Fact]
+    public void TestCreateIncident()
+    {   
+        new IrmCreateIncident().CreateIncident();
     }
 }
