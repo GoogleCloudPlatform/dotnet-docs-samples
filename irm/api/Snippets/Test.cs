20e93892c8b94b67fbc3c2a33a024229508b49ff
Jeffrey Rennie <rennie@google.com>
2019-02-05 12:11:04 -0800

    WIP
    Change-Id: Ieedef7a7604cb0b553373d4de6681a1e2b29f34b

--- irm/api/Snippets/Test.cs
+++ irm/api/Snippets/Test.cs
@@ -1,11 +1,37 @@
 using System;
+using Google.Apis.Auth.OAuth2;
 using Xunit;
 
 public class Test
 {
     [Fact]
     public void TestCreateIncident()
-    {   
-        new IrmCreateIncident().CreateIncident();
+    {
+        new IrmCreateIncident().CreateIncidentWithSignal(ProjectId);
+    }
+
+    [Fact]
+    public void TestCreateSignal()
+    {
+        new IrmCreateSignal().CreateSignal(ProjectId);
+    }
+
+    static readonly string ProjectId = GetProjectId();
+
+    static string GetProjectId()
+    {
+        GoogleCredential googleCredential = Google.Apis.Auth.OAuth2
+            .GoogleCredential.GetApplicationDefault();
+        if (googleCredential != null)
+        {
+            ICredential credential = googleCredential.UnderlyingCredential;
+            ServiceAccountCredential serviceAccountCredential =
+                credential as ServiceAccountCredential;
+            if (serviceAccountCredential != null)
+            {
+                return serviceAccountCredential.ProjectId;
+            }
+        }
+        return Google.Api.Gax.Platform.Instance().ProjectId;
     }
 }
