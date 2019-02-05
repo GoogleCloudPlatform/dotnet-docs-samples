77c366024b916a578eb09e727907f7c02191ebfe
Jeffrey Rennie <rennie@google.com>
2019-02-05 14:31:59 -0800

    Lint

--- irm/api/Snippets/ChangeSeverity.cs
+++ irm/api/Snippets/ChangeSeverity.cs
@@ -1,3 +1,17 @@
+﻿// Copyright (c) 2019 Google LLC.
+// 
+// Licensed under the Apache License, Version 2.0 (the "License"); you may not
+// use this file except in compliance with the License. You may obtain a copy of
+// the License at
+// 
+// http://www.apache.org/licenses/LICENSE-2.0
+// 
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+// WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+// License for the specific language governing permissions and limitations under
+// the License.
+
 using Google.Api.Gax.ResourceNames;
 using Google.Cloud.Irm.V1Alpha2;
 using Google.Protobuf.WellKnownTypes;
@@ -5,14 +19,15 @@ using static Google.Cloud.Irm.V1Alpha2.Incident.Types;
 using System;
 using Google.Api.Gax.Grpc;
 
-class IrmChangeSeverity {
+class IrmChangeSeverity
+{
     public void ChangeSeverity(
         string projectId = "YOUR-PROJECT-ID",
         string incidentId = "What does this look like?")
     {
         // Create client
         IncidentServiceClient incidentServiceClient =
-            IncidentServiceClient.Create();        
+            IncidentServiceClient.Create();
         // Update the severity.
         Incident incidentChange = new Incident()
         {
@@ -20,7 +35,7 @@ class IrmChangeSeverity {
             Severity = Severity.Minor,
         };
         // Tell the API which fields to update.
-        FieldMask mask = new FieldMask() { Paths = {"severity"} };
+        FieldMask mask = new FieldMask() { Paths = { "severity" } };
         // Call the API to update the incident.
         var incident =
             incidentServiceClient.UpdateIncident(incidentChange, mask);
--- irm/api/Snippets/CreateIncident.cs
+++ irm/api/Snippets/CreateIncident.cs
@@ -1,10 +1,25 @@
+﻿// Copyright (c) 2019 Google LLC.
+// 
+// Licensed under the Apache License, Version 2.0 (the "License"); you may not
+// use this file except in compliance with the License. You may obtain a copy of
+// the License at
+// 
+// http://www.apache.org/licenses/LICENSE-2.0
+// 
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+// WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+// License for the specific language governing permissions and limitations under
+// the License.
+
 using Google.Api.Gax.ResourceNames;
 using Google.Cloud.Irm.V1Alpha2;
 using Google.Protobuf.WellKnownTypes;
 using static Google.Cloud.Irm.V1Alpha2.Incident.Types;
 using System;
 
-class IrmCreateIncident {
+class IrmCreateIncident
+{
     public Signal CreateIncidentWithSignal(string projectId = "YOUR-PROJECT-ID")
     {
         // Create client
@@ -14,11 +29,11 @@ class IrmCreateIncident {
         Incident newIncident = new Incident()
         {
             Title = "Somebody pushed the red button!",
-            Synopsis = new Synopsis() 
+            Synopsis = new Synopsis()
             {
-                Author = new User() 
+                Author = new User()
                 {
-                    Email = "janedoe@example.com",                    
+                    Email = "janedoe@example.com",
                 },
                 Content = "Nobody should ever push the red button.",
                 ContentType = "text/plain",
