9d856387d6ed51ec64bb5f255a684b227c964f6b
Jeffrey Rennie <rennie@google.com>
2019-02-05 14:29:43 -0800

    Add retry logic to atomic update.
    Change-Id: I82944d0a671b56bbbdbbf1d61d2ca970517d3138

--- irm/api/Snippets/ChangeStage.cs
+++ irm/api/Snippets/ChangeStage.cs
@@ -5,29 +5,46 @@ using static Google.Cloud.Irm.V1Alpha2.Incident.Types;
 using System;
 using Google.Api.Gax.Grpc;
 
-class IrmChangeStage {
+class IrmChangeStage
+{
     public void ChangeStageAtomically(
         string projectId = "YOUR-PROJECT-ID",
         string incidentId = "What does this look like?")
     {
         // Create client
         IncidentServiceClient incidentServiceClient =
-            IncidentServiceClient.Create();        
-        // Update the severity.
-        Incident incident = incidentServiceClient.GetIncident(
-            new IncidentName(projectId, incidentId).ToString());
-        Incident incidentChange = new Incident()
+            IncidentServiceClient.Create();
+        for (int retry = 0; true; ++retry)
         {
-            Name = incident.Name,
-            Stage = Stage.Detected
-        };
-        // Tell the API which fields to update.
-        FieldMask mask = new FieldMask() { Paths = {"stage"} };
-        // Call the API to update the incident.
-        // Use the "If-Match" header to prevent race conditions.
-        incident = incidentServiceClient.UpdateIncident(incidentChange, mask,
-            CallSettings.FromHeaderMutation(metadata => metadata.Add(
-                "If-Match" , incident.Etag)));
-        Console.WriteLine("Changed stage of {0}.", incident.Name);
+            try
+            {
+                // Observe the current state of the incident.
+                Incident incident = incidentServiceClient.GetIncident(
+                    new IncidentName(projectId, incidentId).ToString());
+                Incident incidentChange = new Incident()
+                {
+                    Name = incident.Name,
+                    Stage = Stage.Detected
+                };
+                // Tell the API which fields to update.
+                FieldMask mask = new FieldMask() { Paths = { "stage" } };
+                // Call the API to update the incident.
+                // Use the "If-Match" header to prevent race conditions.  Only
+                // update the incident if it hasn't changed since we observed
+                // it.
+                incident = incidentServiceClient.UpdateIncident(
+                    incidentChange,
+                    mask,
+                    CallSettings.FromHeaderMutation(metadata => metadata.Add(
+                        "If-Match", incident.Etag)));
+                Console.WriteLine("Changed stage of {0}.", incident.Name);
+            }
+            // TODO: narrow down the exception type.
+            catch (Exception) when (retry < 3)
+            {
+                // Somebody else must have updated the incident at the same 
+                // time!  Try again.
+            }
+        }
     }
 }
