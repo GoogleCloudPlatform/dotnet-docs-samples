1d006d3ba807bee7bfe9a58faf393827ced057f4
Frank Natividad <franknatividad@google.com>
2019-03-15 20:38:46 -0700

    a little more testing

--- storage/api/StorageTest/StorageTest.cs
+++ storage/api/StorageTest/StorageTest.cs
@@ -718,6 +718,8 @@ namespace GoogleCloudSamples
             {
                 var enableBucketPolicyOnly = Run("enable-bucket-policy-only", bucketPolicyOnly.BucketName);
                 AssertSucceeded(enableBucketPolicyOnly);
+                var getBucketPolicyOnly = Run("get-bucket-policy-only", bucketPolicyOnly.BucketName);
+                AssertSucceeded(getBucketPolicyOnly);
                 var disableBucketPolicyOnly = Run("disable-bucket-policy-only", bucketPolicyOnly.BucketName);
                 AssertSucceeded(disableBucketPolicyOnly);
                 var getBucketPolicyOnly = Run("get-bucket-policy-only", bucketPolicyOnly.BucketName);
