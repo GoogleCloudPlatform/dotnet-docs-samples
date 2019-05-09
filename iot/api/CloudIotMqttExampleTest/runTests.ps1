fdc85136878413048e80c792fe86dadf2e4d374c
Jeffrey Rennie <rennie@google.com>
2019-05-09 08:56:32 -0700

    Skip Iot mqtt test until it gets fixed.
    Change-Id: I523edf5a11972e1db3d33611e48306bc21d1d04d

--- iot/api/CloudIotMqttExampleTest/runTests.ps1
+++ iot/api/CloudIotMqttExampleTest/runTests.ps1
@@ -11,4 +11,8 @@
 # WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
 # License for the specific language governing permissions and limitations under
 # the License.
+Import-Module ../../../BuildTools.psm1 -DisableNameChecking
+
+Skip-Test
+
 dotnet test --test-adapter-path:. --logger:junit
