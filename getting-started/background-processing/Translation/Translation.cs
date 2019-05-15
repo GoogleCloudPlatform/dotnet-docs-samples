a107116c8edc11585b28279cd86fd42e1cfc3e2e
Jeffrey Rennie <rennie@google.com>
2019-05-15 18:12:28 -0400

    Build script builds and registers container.

--- getting-started/background-processing/Translation/Translation.cs
+++ getting-started/background-processing/Translation/Translation.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using Google.Cloud.Firestore;
 
+/// <summary>
+/// The record that gets stored in Firestore.
+/// </summary>
 [FirestoreData]
 public class Translation
 {
---
+++ getting-started/background-processing/TranslatorWorker/BuildAndDeploy.ps1
@@ -0,0 +1,7 @@
+# Build the application locally.
+dotnet publish -c Release
+# Use Google Cloud Build to build the container and publish to Google
+# Container Registry. 
+Copy-Item Dockerfile bin/Release/netcoreapp2.2/publish
+$projectId = gcloud config get-value project
+gcloud builds submit --tag gcr.io/$projectId/translator-worker bin/Release/netcoreapp2.2/publish
