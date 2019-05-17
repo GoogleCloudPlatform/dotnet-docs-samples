4c5f1a16fe35b8aac177b8df9a33c4924354611f
Jeffrey Rennie <rennie@google.com>
2019-05-17 11:20:32 -0700

    Helloworld sample to try Cloud Run.
    Change-Id: I94668a81b08af8188338d39f0383bf664498dacb

---
+++ getting-started/background-processing/run-helloworld/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+
+namespace run_helloworld
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateWebHostBuilder(args).Build().Run();
+        }
+
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+        
+            WebHost.CreateDefaultBuilder(args)
+                .UseStartup<Startup>().UsePortEnvironmentVariable();
+    }
+
+    static class ProgramExtensions
+    {
+        public static IWebHostBuilder UsePortEnvironmentVariable(
+            this IWebHostBuilder builder)
+        {
+            string port = Environment.GetEnvironmentVariable("PORT");
+            if (!string.IsNullOrEmpty(port)) {
+                builder.UseUrls($"http://0.0.0.0:{port}");
+            }
+            return builder;
+        }
+    }
+}
