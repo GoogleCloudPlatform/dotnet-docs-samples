b2ad7ad2e8986a9bf06534c8b4107279e307ea6f
Jeffrey Rennie <rennie@google.com>
2019-05-20 19:11:18 -0400

    Lint, and more log messages.

--- getting-started/background-processing/TranslateWorker/Startup.cs
+++ getting-started/background-processing/TranslateWorker/Startup.cs
@@ -1,4 +1,18 @@
-﻿using Google.Apis.Auth.OAuth2;
+﻿// Copyright (c) 2019 Google LLC.
+// 
+// Licensed under the Apache License, Version 2.0 (the "License"); you may not
+// use this file except in compliance with the License. You may obtain a copy of
+// the License at
+// 
+// http://www.apache.org/licenses/LICENSE-2.0
+// 
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+// WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+// License for the specific language governing permissions and limitations under
+// the License.
+
+using Google.Apis.Auth.OAuth2;
 using Google.Cloud.Firestore;
 using System;
 using System.Collections.Generic;
@@ -29,7 +43,7 @@ namespace TranslateWorker
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddSingleton<FirestoreDb>(provider => 
+            services.AddSingleton<FirestoreDb>(provider =>
                 FirestoreDb.Create(GetFirestoreProjectId()));
             services.AddSingleton<Google.Cloud.Translation.V2.TranslationClient>(
                 Google.Cloud.Translation.V2.TranslationClient.Create());
@@ -67,7 +81,7 @@ namespace TranslateWorker
                 {
                     return serviceAccountCredential.ProjectId;
                 }
-            }            
+            }
             // Query the metadata server.
             HttpResponseMessage response = new HttpClient().GetAsync(
                 @"http://metadata.google.internal/computeMetadata/v1/project/project-id").Result;
--- getting-started/background-processing/TranslatorWorker/Worker.cs
+++ getting-started/background-processing/TranslatorWorker/Worker.cs
@@ -1,4 +1,18 @@
-﻿using System;
+﻿// Copyright (c) 2019 Google LLC.
+// 
+// Licensed under the Apache License, Version 2.0 (the "License"); you may not
+// use this file except in compliance with the License. You may obtain a copy of
+// the License at
+// 
+// http://www.apache.org/licenses/LICENSE-2.0
+// 
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+// WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+// License for the specific language governing permissions and limitations under
+// the License.
+
+using System;
 using System.Threading.Tasks;
 using Google.Apis.Auth.OAuth2;
 using Google.Cloud.Firestore;
@@ -46,11 +60,11 @@ namespace TranslatorWorker
             string sourceText = message.Data.ToStringUtf8();
             var result = await _translator.TranslateTextAsync(sourceText, "es");
             // Store the result in Firestore.
-            Translation translation = new Translation() 
+            Translation translation = new Translation()
             {
                 TimeStamp = DateTime.UtcNow,
                 SourceText = sourceText,
-                TranslatedText = result.TranslatedText                
+                TranslatedText = result.TranslatedText
             };
             await _translations.AddAsync(translation);
             // Acknowledge this Pub/Sub message so we never see it again.
@@ -83,6 +97,5 @@ namespace TranslatorWorker
         public string GetSubscriptionId() =>
             System.Environment.GetEnvironmentVariable(
                 "TranslateRequestsSubscriptionId") ?? "translate-requests";
-
     }
 }
