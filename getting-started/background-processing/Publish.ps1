4b64946cd237dfef63f953453ae84a31f63d4b1d
Jeffrey Rennie <rennie@google.com>
2019-05-20 16:21:33 -0400

    No error messages when the topic or subscription already exist.

--- getting-started/background-processing/Publish.ps1
+++ getting-started/background-processing/Publish.ps1
@@ -4,13 +4,26 @@ dotnet publish -c Release
 # Use Google Cloud Build to build the worker's container and publish to Google
 # Container Registry. 
 $projectId = gcloud config get-value project
-gcloud builds submit --tag gcr.io/$projectId/translate-worker TranslateWorker/bin/Release/netcoreapp2.2/publish
+gcloud builds submit --tag gcr.io/$projectId/translate-worker `
+    TranslateWorker/bin/Release/netcoreapp2.2/publish
 
 # Run the container with Google Cloud Run.
 $region = "us-central1"
-gcloud beta run deploy translate-worker --region $region --image gcr.io/$projectId/translate-worker --no-allow-unauthenticated
-$url = gcloud beta run services describe translate-worker --region $region --format="get(status.address.hostname)"
+gcloud beta run deploy translate-worker --region $region `
+    --image gcr.io/$projectId/translate-worker --no-allow-unauthenticated
+$url = gcloud beta run services describe translate-worker `
+    --region $region --format="get(status.address.hostname)"
 
-# Create a pub/sub topic and subscription.
-gcloud pubsub topics create translate-requests
-gcloud pubsub subscriptions create translate-requests --topic translate-requests --push-endpoint $url/api/translate
+# Create a pub/sub topic and subscription, if they don't already exist.
+$topicExists = gcloud pubsub topics describe translate-requests 2> $null 
+if (-not $topicExists) {
+    gcloud pubsub topics create translate-requests
+}
+$subscriptionExists = gcloud pubsub subscriptions describe translate-requests 2> $null
+if ($subscriptionExists) {
+    gcloud pubsub subscriptions modify-push-config translate-requests `
+        --push-endpoint $url/api/translate
+} else {
+    gcloud pubsub subscriptions create translate-requests `
+        --topic translate-requests --push-endpoint $url/api/translate
+}
