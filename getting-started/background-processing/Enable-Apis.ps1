0ec4e191e9fd7e23d8e48a43c4eb974a470c595b
Jeffrey Rennie <rennie@google.com>
2019-05-22 14:03:25 -0700

    gcloud services enable can't handle multiline arguments.

--- getting-started/background-processing/Enable-Apis.ps1
+++ getting-started/background-processing/Enable-Apis.ps1
@@ -12,10 +12,7 @@
 # License for the specific language governing permissions and limitations under
 # the License.
 
-$servicesToEnable = @"
-translate.googleapis.com
-pubsub.googleapis.com
-firestore.googleapis.com
-cloudrun.googleapis.com
-"@
-gcloud services enable $servicesToEnable
+gcloud services enable $servicesToEnable translate.googleapis.com `
+    pubsub.googleapis.com firestore.googleapis.com run.googleapis.com `
+    run.googleapis.com
+
