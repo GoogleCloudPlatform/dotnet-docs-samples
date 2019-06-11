6138c9e317f452f82d9a9ad5c25a0c8ad7c58233
Simon Zeltser <zeltser@google.com>
2019-06-10 22:41:51 -0700

    Fix build tools path

--- applications/liftAndShiftPetshop/runTests.ps1
+++ applications/liftAndShiftPetshop/runTests.ps1
@@ -12,7 +12,7 @@
 # WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
 # License for the specific language governing permissions and limitations under
 # the License.
-Import-Module ..\..\..\BuildTools.psm1 -DisableNameChecking
+Import-Module ..\..\BuildTools.psm1 -DisableNameChecking
 
 Require-Platform Win*
 
