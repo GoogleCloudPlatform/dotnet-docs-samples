5bf64f0787bbe418ae7fe09928c71f8322d4dd1b
Jeff Williams <jefesaurus@google.com>
2019-06-20 14:29:05 -0700

    Spanner: Prevent crash when an Album has a null MarketingBudget
    If you're working through the "Getting started in C#" docs [1] from start to finish, then you'll see a crash when you run `dotnet run queryDataWithIndex`. That's because one of the albums in the query results has a null MarketingBudget, and QueryDataWithIndexAsync() tries to format the DBNull as a string. (I'm using .NET Core on a Mac; I don't know whether this happens with .NET Framework on Windows.)
    
    This change checks for a null MarketingBudget and prints it as an empty string. It also adds a test for QueryDataWithIndexAsync().
    
    [1]: https://cloud.google.com/spanner/docs/getting-started/csharp/

--- spanner/api/Spanner/Program.cs
+++ spanner/api/Spanner/Program.cs
@@ -706,12 +706,16 @@ namespace GoogleCloudSamples.Spanner
                 {
                     while (await reader.ReadAsync())
                     {
+                        var marketingBudget = reader.IsDBNull(
+                            reader.GetOrdinal("MarketingBudget")) ?
+                            "" :
+                            reader.GetFieldValue<string>("MarketingBudget");
                         Console.WriteLine("AlbumId : "
                         + reader.GetFieldValue<string>("AlbumId")
                         + " AlbumTitle : "
                         + reader.GetFieldValue<string>("AlbumTitle")
                         + " MarketingBudget : "
-                        + reader.GetFieldValue<string>("MarketingBudget"));
+                        + marketingBudget);
                     }
                 }
             }
@@ -1610,7 +1614,7 @@ namespace GoogleCloudSamples.Spanner
         }
         // [END spanner_dml_batch_update]
 
-        // [START spanner_delete_data]	    
+        // [START spanner_delete_data]
         public static async Task DeleteSampleDataAsync(
             string projectId, string instanceId, string databaseId)
         {
@@ -2338,7 +2342,7 @@ namespace GoogleCloudSamples.Spanner
                 + $"/databases/{databaseId}";
             using (var connection = new SpannerConnection(connectionString))
             {
-                // Define create table statement for table with 
+                // Define create table statement for table with
                 // commit timestamp column.
                 string createTableStatement =
                 @"CREATE TABLE Performances (
