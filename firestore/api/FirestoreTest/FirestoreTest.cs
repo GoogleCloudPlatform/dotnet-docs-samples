28fbe1c06e895ca0c88c053a03987d3afa0a9595
BenWhitehead <BenWhitehead@users.noreply.github.com>
2019-08-19 12:19:29 -0400

    Fix incorrect assertion

--- firestore/api/FirestoreTest/FirestoreTest.cs
+++ firestore/api/FirestoreTest/FirestoreTest.cs
@@ -357,7 +357,7 @@ namespace GoogleCloudSamples
         public void UpdateDocumentIncrementTest()
         {
             RunQueryData("update-document-increment", Environment.GetEnvironmentVariable("FIRESTORE_PROJECT_ID"));
-            var output = RunAddData("update-document-array", Environment.GetEnvironmentVariable("FIRESTORE_PROJECT_ID"));
+            var output = RunAddData("update-document-increment", Environment.GetEnvironmentVariable("FIRESTORE_PROJECT_ID"));
             Assert.Contains("Updated the population of the DC document in the cities collection.", output.Stdout);
         }
 
