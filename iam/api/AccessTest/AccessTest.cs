b17aa35e7394f90cd823f3c9c646a72e3dd5e1f1
mdejong3 <35782177+mdejong3@users.noreply.github.com>
2019-08-21 14:53:31 -0700

    Add tests to AccessTest.cs
    Added tests for AddBinding, AddMember, and RemoveMember

--- iam/api/AccessTest/AccessTest.cs
+++ iam/api/AccessTest/AccessTest.cs
@@ -1,4 +1,4 @@
-// Copyright 2018 Google Inc.
+// Copyright 2019 Google Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -19,12 +19,48 @@ namespace GoogleCloudSamples
 {
     public class AccessTest
     {
+        private readonly string _project;
+        private readonly string _role1;
+        private readonly string _role2;
+        private readonly string _member1;
+        private readonly string _member2; 
+        private readonly string _member3;
+
+         public AccessTest(){
+            _project = Environment.GetEnvironmentVariable("GOOGLE_PROJECT_ID");
+            _role1 = "roles/viewer";
+            _role2 = "roles/editor";
+            _member1 = "user:user1@example.com";
+            _member2 = "user:user2@example.com";
+            _member3 = "user:user3@example.com";
+        }
+
         [Fact]
         public void TestAccess()
         {
-            string project = Environment.GetEnvironmentVariable("GOOGLE_PROJECT_ID");
-            var policy = AccessManager.GetPolicy(project);
-            policy = AccessManager.SetPolicy(project, policy);
+            // Test GetPolicy
+            var policy = AccessManager.GetPolicy(_project);
+
+            // Test AddBinding
+            policy = AccessManager.AddBinding(policy, _role1, _member1);
+
+            // Test AddMember
+            policy = AccessManager.AddMember(policy, _role1, _member2);
+
+            // Test RemoveMember where role binding doesn't exist
+            policy = AccessManager.RemoveMember(policy, _role2, _member1);
+
+            // Test RemoveMember where member doesn't exist
+            policy = AccessManager.RemoveMember(policy, _role1, _member3);
+
+            // Test RemoveMember
+            policy = AccessManager.RemoveMember(policy, _role1, _member1);
+
+            // Test RemoveMember when removing last member from binding
+            policy = AccessManager.RemoveMember(policy, _role1, _member2);
+
+            // Test SetPolicy
+            policy = AccessManager.SetPolicy(_project, policy);
         }
     }
 }
