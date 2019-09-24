c1e6e6a1884883439384bc1adee5921b488858fb
Jonathan Lui <jonathanlui@google.com>
2019-09-24 13:17:40 -0700

    move Storage tests to *2

--- .kokoro/main.ps1
+++ .kokoro/main.ps1
@@ -34,7 +34,7 @@ try {
     $groups = @(
         $false,  # 0: Everything.
         $false,  # 1: Everything not in another group.
-        @('video', 'applications')  # 2
+        @('video', 'applications', 'storage')  # 2
     )
 
     $union = $groups[2..($groups.Length-1)] | Select-Object
