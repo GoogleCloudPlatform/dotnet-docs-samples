c8d9b87b26e377c329e461f63e08d85117ef0812
Munkhbayar Ganbold <munkhbayarg@google.com>
2019-11-14 15:06:11 -0800

    completed all TO-DOs

--- automl/api/Snippets/Program.cs
+++ automl/api/Snippets/Program.cs
@@ -11,15 +11,10 @@
 // WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
 // License for the specific language governing permissions and limitations under
 // the License.
-<<<<<<< HEAD
-
-=======
->>>>>>> 51eed317... Adds code snippets for AutoML Translation: create dataset, create model, predict.
 using CommandLine;
 
 namespace GoogleCloudSamples
 {
-<<<<<<< HEAD
     public class BaseOptions
     {
         [Value(0, HelpText = "Your project ID")]
@@ -27,8 +22,6 @@ namespace GoogleCloudSamples
     }
 
 
-=======
->>>>>>> 51eed317... Adds code snippets for AutoML Translation: create dataset, create model, predict.
     [Verb("create_dataset", HelpText = "Create a new dataset for training a model")]
     public class CreateDatasetOptions
     {
@@ -41,11 +34,7 @@ namespace GoogleCloudSamples
 
     [Verb("create_model", HelpText = "Create a new custom model")]
     public class CreateModelOptions
-<<<<<<< HEAD
     {
-=======
-    { 
->>>>>>> 51eed317... Adds code snippets for AutoML Translation: create dataset, create model, predict.
         [Value(0, HelpText = "Your project ID")]
         public string ProjectID { get; set; }
 
@@ -56,7 +45,6 @@ namespace GoogleCloudSamples
         public string DatasetID { get; set; }
     }
 
-<<<<<<< HEAD
     [Verb("get_model", HelpText = "Retrieve a model for AutoML")]
     public class GetModelOptions
     {
@@ -67,8 +55,6 @@ namespace GoogleCloudSamples
         public string ModelId { get; set; }
     }
 
-=======
->>>>>>> 51eed317... Adds code snippets for AutoML Translation: create dataset, create model, predict.
     [Verb("predict", HelpText = "Make a prediction with a custom model")]
     public class PredictOptions
     {
