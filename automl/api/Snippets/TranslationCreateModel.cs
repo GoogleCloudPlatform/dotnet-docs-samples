72ecfeb1840a17ac7ce3e0e9d2368e74f86affb0
Eric Schmidt <erschmid@google.com>
2019-11-14 18:48:39 -0800

    Adds code snippets for AutoML Translation: create dataset, create model, predict.

---
+++ automl/api/Snippets/TranslationCreateModel.cs
@@ -0,0 +1,68 @@
+﻿// Copyright (c) 2019 Google LLC.
+//
+// Licensed under the Apache License, Version 2.0 (the "License"); you may not
+// use this file except in compliance with the License. You may obtain a copy of
+// the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+// WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+// License for the specific language governing permissions and limitations under
+// the License.
+using CommandLine;
+using Google.Cloud.AutoML.V1;
+using System;
+
+namespace GoogleCloudSamples
+{
+
+    [Verb("create_translation_model", HelpText = "Create a new custom AutoML Translation model")]
+    public class AutoMLTranslationCreateModelOptions : CreateModelOptions
+    { }
+
+    public class AutoMLTranslationCreateModel
+    {
+        public static object TranslationCreateModel(string projectID,
+                                                 string displayName,
+                                                 string datasetID)
+        {
+
+            // Initialize client that will be used to send requests. This client only needs to be created
+            // once, and can be reused for multiple requests. After completing all of your requests, call
+            // the "close" method on the client to safely clean up any remaining background resources.
+            var client = AutoMlClient.Create();
+
+            // A resource that represents Google Cloud Platform location.
+            var locationName = LocationName.Format(projectID, "us-central1");
+
+            var modelRequest = new Model
+            {
+                DisplayName = displayName,
+                DatasetId = datasetID,
+                TranslationModelMetadata = new TranslationModelMetadata()
+            };
+
+            // Create a model with the model metadata in the region.
+            var response = client.CreateModel(locationName, modelRequest);
+
+            // Don't wait for model creation to finish, as this can take several hours.
+            // However, you can use the `name` of the operation to check the status of your model.
+
+            Console.WriteLine($"Training operation name: {response.Name}");
+            Console.WriteLine("Training started...");
+
+            return 0;
+        }
+
+        public static void RegisterCommands(VerbMap<object> verbMap)
+        {
+            verbMap
+                .Add((AutoMLTranslationCreateModelOptions opts) =>
+                     AutoMLTranslationCreateModel.TranslationCreateModel(opts.ProjectID,
+                                                                         opts.DisplayName,
+                                                                         opts.DatasetID));
+        }
+    }
+}
