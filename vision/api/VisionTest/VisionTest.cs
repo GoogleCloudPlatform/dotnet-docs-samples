4c4eaad4c1ae0400f4c7fd822492e858b9ee009c
Eric Schmidt <erschmid@google.com>
2019-12-03 09:25:18 -0800

    fixed the order of arguments passed to test

--- vision/api/VisionTest/VisionTest.cs
+++ vision/api/VisionTest/VisionTest.cs
@@ -132,7 +132,7 @@ namespace GoogleCloudSamples
         [Fact]
         public void DetectTextWithMultiRegion()
         {
-            var output = Run("text", "-m", Path.Combine("data", "bonito.gif"));
+            var output = Run("text", Path.Combine("data", "bonito.gif"), "-m");
             Assert.Equal(0, output.ExitCode);
             Assert.Contains("fermented", output.Stdout);
         }
