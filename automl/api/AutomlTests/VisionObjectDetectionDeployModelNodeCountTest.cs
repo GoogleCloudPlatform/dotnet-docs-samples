eaf2c86fa959e0627da2950bcc70642cdbc11064
Munkhbayar Ganbold <munkhbayarg@google.com>
2020-01-06 12:04:33 -0800

    refactored model tests

---
+++ automl/api/AutomlTests/VisionObjectDetectionDeployModelNodeCountTest.cs
@@ -0,0 +1,51 @@
+// Copyright 2019 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+using Grpc.Core;
+using System;
+using System.IO;
+using System.Threading;
+using Xunit;
+
+namespace GoogleCloudSamples
+{
+    [Collection(nameof(AutoMLFixture))]
+    public class VisionObjectDetectionDeployModelNodeCountTest
+    {
+        private readonly AutoMLFixture _fixture;
+        private readonly string _modelId = "IOD0000000000000000000";
+        public VisionObjectDetectionDeployModelNodeCountTest(AutoMLFixture fixture)
+        {
+            _fixture = fixture;
+        }
+
+        [Fact]
+        public void TestDeployModelWithNodeCount()
+        {
+            // As model deployment can take a long time, instead try to deploy a
+            // nonexistent model and confirm that the model was not found, but other
+            // elements of the request were valid.
+            try
+            {
+                ConsoleOutput output = _fixture.SampleRunner.Run("vision_object_detection_deploy_model_node_count", _fixture.ProjectId, _modelId);
+                Assert.Contains("The model does not exist", output.Stdout);
+            }
+            catch (Exception ex) when (ex is ThreadInterruptedException ||
+                 ex is IOException || ex is RpcException || ex is AggregateException)
+            {
+                Assert.Contains("The model does not exist", ex.Message);
+            }
+        }
+    }
+}
