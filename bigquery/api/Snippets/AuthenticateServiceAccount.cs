291482cfaee22638eb8e2a250c2098c2a4972acf
Eric Schmidt <erschmid@google.com>
2020-01-14 14:16:03 -0800

    feat: adds explicit authentication with JSON credentials to BQ snippets

---
+++ bigquery/api/Snippets/AuthenticateServiceAccount.cs
@@ -0,0 +1,32 @@
+﻿using Google.Apis.Auth.OAuth2;
+using Google.Cloud.BigQuery.V2;
+using System;
+
+public class BigQueryAuthenticateServiceAccount
+{
+    public void AuthenticateWithServiceAccount(string projectId,
+                                               string jsonPath)
+    {
+        // [START bigquery_client_json_credentials]
+        var credentials = GoogleCredential.FromFile(jsonPath);
+        var client = BigQueryClient.Create(projectId, credentials);
+        // [END bigquery_client_json_credentials]
+
+        // Use the client
+        string query = @"
+            SELECT name FROM `bigquery-public-data.usa_names.usa_1910_2013`
+            WHERE state = 'TX'
+            LIMIT 100";
+        BigQueryJob job = client.CreateQueryJob(
+            sql: query,
+            parameters: null,
+            options: new QueryOptions { UseQueryCache = false });
+        // Wait for the job to complete.
+        job = job.PollUntilCompleted().ThrowOnAnyError();
+        // Display the results
+        foreach (BigQueryRow row in client.GetQueryResults(job.Reference))
+        {
+            Console.WriteLine($"{row["name"]}");
+        }
+    }
+}
--- bigquery/api/Snippets/Test.cs
+++ bigquery/api/Snippets/Test.cs
@@ -27,6 +27,7 @@ using System.Linq;
 public class BigQueryTest : IDisposable, IClassFixture<RandomBucketFixture>
 {
     private readonly string _projectId;
+    private readonly string _jsonPath;
     private readonly BigQueryClient _client;
     readonly List<BigQueryDataset> _tempDatasets = new List<BigQueryDataset>();
     readonly StorageClient _storage;
@@ -37,11 +38,21 @@ public class BigQueryTest : IDisposable, IClassFixture<RandomBucketFixture>
     public BigQueryTest(RandomBucketFixture randomBucketFixture)
     {
         _projectId = Environment.GetEnvironmentVariable("GOOGLE_PROJECT_ID");
+        _jsonPath = System.Environment.GetEnvironmentVariable("GOOGLE_APPLICATION_CREDENTIALS");
         _client = BigQueryClient.Create(_projectId);
         _storage = StorageClient.Create();
         _bucketName = randomBucketFixture.BucketName;
         _stringOut = new StringWriter();
         Console.SetOut(_stringOut);
+    }
+
+    [Fact]
+    public void TestAuthenticateServiceAccount()
+    {
+        var snippet = new BigQueryAuthenticateServiceAccount();
+        snippet.AuthenticateWithServiceAccount(_projectId, _jsonPath);
+        var outputLines = _stringOut.ToString().Trim().Split(new[] { '\n' });
+        Assert.Equal(100, outputLines.Count());
     }
 
     [Fact]
