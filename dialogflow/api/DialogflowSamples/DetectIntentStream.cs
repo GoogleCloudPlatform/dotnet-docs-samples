92f6241eaae9a184d7fb7c5d8997919bbc966c07
Eric Schmidt <erschmid@google.com>
2020-01-17 10:08:39 -0800

    Addressed reviewer comments; fixed linter errors

--- dialogflow/api/DialogflowSamples/DetectIntentStream.cs
+++ dialogflow/api/DialogflowSamples/DetectIntentStream.cs
@@ -1,9 +1,23 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.IO;
-using System.Threading.Tasks;
+// Copyright(c) 2020 Google Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License"); you may not
+// use this file except in compliance with the License. You may obtain a copy of
+// the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+// WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+// License for the specific language governing permissions and limitations under
+// the License.
+
 using CommandLine;
 using Google.Cloud.Dialogflow.V2;
+using System.IO;
+using System.Threading.Tasks;
 
 namespace GoogleCloudSamples
 {
@@ -24,9 +38,10 @@ namespace GoogleCloudSamples
         }
 
         // [START dialogflow_detect_intent_streaming]
-        public static async Task<object> DetectIntentFromStream(string projectId,
-                                                 string sessionId,
-                                                 string filePath)
+        public static async Task<object> DetectIntentFromStreamAsync(
+            string projectId,
+            string sessionId,
+            string filePath)
         {
             var sessionsClient = SessionsClient.Create();
             var sessionName = SessionName.Format(projectId, sessionId);
