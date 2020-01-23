edcb9d4db907ac9a6594bec6e9847848f70d9290
Eric Schmidt <erschmid@google.com>
2020-01-23 10:38:46 -0800

    Specifies UTF-8 encoding for output of Translation response (e.g. for non-Latin orthographies). (#937)
    This affects the following code snippets:
    
    - translate_translate_text
    - translate_quickstart

--- translate/api/QuickStart/Program.cs
+++ translate/api/QuickStart/Program.cs
@@ -21,7 +21,7 @@ public class QuickStart
 {
     public static void Main(string[] args)
     {
-        Console.OutputEncoding = System.Text.Encoding.Unicode;
+        Console.OutputEncoding = System.Text.Encoding.UTF8;
         TranslationClient client = TranslationClient.Create();
         var response = client.TranslateText("Hello World.", "ru");
         Console.WriteLine(response.TranslatedText);
