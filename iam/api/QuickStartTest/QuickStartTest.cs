da8274daaf3d886206f7940f438c816d7d1d988b
Melanie DeJong <melaniedejong@google.com>
2020-03-03 17:02:30 -0800

    Restoring old version of QuickStartTest

--- iam/api/QuickStartTest/QuickStartTest.cs
+++ iam/api/QuickStartTest/QuickStartTest.cs
@@ -3,26 +3,6 @@ using Xunit;
 
 public class QuickStartTest
 {
-    private readonly string _project;
-    private readonly string _member;
-    private readonly string _role;
-    
-    public QuickStartTest()
-    {
-        _project = Environment.GetEnvironmentVariable("GOOGLE_PROJECT_ID");
-        _member = "user:YaraArryn.677500@gmail.com";
-        _role = "roles/logging.viewer";
-    }
-        
-    [Fact]
-    public void TestQuickStart()
-    {
-        // Will throw exceptions on failure
-        var crmService = QuickStartAddMember.InitializeService();
-        QuickStartAddMember.AddBinding(crmService, _project, _member, _role);
-        QuickStartRemoveMember.RemoveMember(crmService, _project, _member, _role);
-    }
-
     [Fact]
     public void TestMain()
     {
