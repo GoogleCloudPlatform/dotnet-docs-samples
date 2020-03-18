f897d7dcdc57d79c8694d87eb348966db17ac504
Hugo Sanchez <sanchezhugo.1994@gmail.com>
2020-03-18 16:31:20 -0700

    Remove namespaces

--- securitycenter/api/src/ListNotificationConfigSnippets.g.cs
+++ securitycenter/api/src/ListNotificationConfigSnippets.g.cs
@@ -14,43 +14,40 @@
  * limitations under the License.
  */
 
-namespace Snippets
+// [START scc_list_notification_configs]
+using Google.Cloud.SecurityCenter.V1;
+using Google.Api.Gax.ResourceNames;
+using Google.Protobuf;
+using Google.Protobuf.WellKnownTypes;
+using Grpc.Core;
+using System;
+using System.Collections;
+using System.Collections.Immutable;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+// [END scc_list_notification_configs]
+
+/** Snippets for how to ListNotificationConfig. */
+public class ListNotificationConfigSnippets
 {
+    private ListNotificationConfigSnippets() {}
+
+    public static readonly string projectId = Environment.GetEnvironmentVariable("GOOGLE_PROJECT_ID");
     // [START scc_list_notification_configs]
-    using Google.Cloud.SecurityCenter.V1;
-    using Google.Api.Gax.ResourceNames;
-    using Google.Protobuf;
-    using Google.Protobuf.WellKnownTypes;
-    using Grpc.Core;
-    using System;
-    using System.Collections;
-    using System.Collections.Immutable;
-    using System.Collections.Generic;
-    using System.Collections.ObjectModel;
-    using System.Linq;
-    using System.Threading;
-    using System.Threading.Tasks;
-    // [END scc_list_notification_configs]
-
-    /** Snippets for how to ListNotificationConfig. */
-    public class ListNotificationConfigSnippets
+    public static ImmutableList<NotificationConfig> listNotificationConfigs(String organizationId)
     {
-        private ListNotificationConfigSnippets() {}
+        // String organizationId = "{your-org-id}";
+        OrganizationName orgName = new OrganizationName(organizationId);
+        SecurityCenterClient client = SecurityCenterClient.Create();
+        Google.Api.Gax.PagedEnumerable<Google.Cloud.SecurityCenter.V1.ListNotificationConfigsResponse, Google.Cloud.SecurityCenter.V1.NotificationConfig> response = client.ListNotificationConfigs(orgName);
+        ImmutableList<NotificationConfig> notificationConfigs = response.ReadPage(50).ToImmutableList();
 
-        public static readonly string projectId = Environment.GetEnvironmentVariable("GOOGLE_PROJECT_ID");
-        // [START scc_list_notification_configs]
-        public static ImmutableList<NotificationConfig> listNotificationConfigs(String organizationId)
-        {
-            // String organizationId = "{your-org-id}";
-            OrganizationName orgName = new OrganizationName(organizationId);
-            SecurityCenterClient client = SecurityCenterClient.Create();
-            Google.Api.Gax.PagedEnumerable<Google.Cloud.SecurityCenter.V1.ListNotificationConfigsResponse, Google.Cloud.SecurityCenter.V1.NotificationConfig> response = client.ListNotificationConfigs(orgName);
-            ImmutableList<NotificationConfig> notificationConfigs = response.ReadPage(50).ToImmutableList();
-            
-            // Print Notification Configuration names.
-            notificationConfigs.ForEach(config => Console.WriteLine(config.NotificationConfigName));
-            return notificationConfigs;
-            }
-    }
-        // [END scc_list_notification_configs]
+        // Print Notification Configuration names.
+        notificationConfigs.ForEach(config => Console.WriteLine(config.NotificationConfigName));
+        return notificationConfigs;
+        }
 }
+// [END scc_list_notification_configs]
--- securitycenter/api/tests/SecurityCenterTests.cs
+++ securitycenter/api/tests/SecurityCenterTests.cs
@@ -1,66 +1,78 @@
+/*
+ * Copyright 2020 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 using System;
 using Xunit;
 using Snippets;
 
-namespace Tests
+public class SecurityCenterTests
 {
-    
-    public class SecurityCenterTests
+    [Fact]
+    public void CreateNotificationConfig_ShouldCreateConfig()
     {
-        [Fact]
-        public void CreateNotificationConfig_ShouldCreateConfig()
-        {
-            String configId = "csharp-create-config-test111";
-            Assert.NotNull(CreateNotificationConfigSnippet.createNotificationConfig(GetOrganizationId(), configId, GetProjectId(), GetTopic()));
-            DeleteNotificationConfigSnippets.deleteNotificationConfig(GetOrganizationId(), configId);
-            Assert.True(true);
-        }
+        String configId = "csharp-create-config-test111";
+        Assert.NotNull(CreateNotificationConfigSnippet.createNotificationConfig(GetOrganizationId(), configId, GetProjectId(), GetTopic()));
+        DeleteNotificationConfigSnippets.deleteNotificationConfig(GetOrganizationId(), configId);
+        Assert.True(true);
+    }
 
-        [Fact]
-        public void DeleteNotificationConfig_ShouldDeleteConfig()
-        {
-            String configId = "csharp-delete-config-test111";
-            CreateNotificationConfigSnippet.createNotificationConfig(GetOrganizationId(), configId, GetProjectId(), GetTopic());
-            Assert.True(DeleteNotificationConfigSnippets.deleteNotificationConfig(GetOrganizationId(), configId));
-        }
+    [Fact]
+    public void DeleteNotificationConfig_ShouldDeleteConfig()
+    {
+        String configId = "csharp-delete-config-test111";
+        CreateNotificationConfigSnippet.createNotificationConfig(GetOrganizationId(), configId, GetProjectId(), GetTopic());
+        Assert.True(DeleteNotificationConfigSnippets.deleteNotificationConfig(GetOrganizationId(), configId));
+    }
 
-        [Fact]
-        public void ListNotificationConfig_ShouldReturnNonEmpty()
-        {
-            String configId = "csharp-list-config-test111";
-            CreateNotificationConfigSnippet.createNotificationConfig(GetOrganizationId(), configId, GetProjectId(), GetTopic());
-            Assert.NotNull(ListNotificationConfigSnippets.listNotificationConfigs(GetOrganizationId()));
-            DeleteNotificationConfigSnippets.deleteNotificationConfig(GetOrganizationId(), configId);
-        }
+    [Fact]
+    public void ListNotificationConfig_ShouldReturnNonEmpty()
+    {
+        String configId = "csharp-list-config-test111";
+        CreateNotificationConfigSnippet.createNotificationConfig(GetOrganizationId(), configId, GetProjectId(), GetTopic());
+        Assert.NotNull(ListNotificationConfigSnippets.listNotificationConfigs(GetOrganizationId()));
+        DeleteNotificationConfigSnippets.deleteNotificationConfig(GetOrganizationId(), configId);
+    }
 
-        [Fact]
-        public void GetNotificationConfig_ShouldReturnConfig()
-        {
-            String configId = "csharp-get-config-test111";
-            CreateNotificationConfigSnippet.createNotificationConfig(GetOrganizationId(), configId, GetProjectId(), GetTopic());
-            Assert.NotNull(GetNotificationConfigSnippets.getNotificationConfig(GetOrganizationId(), configId));
-            DeleteNotificationConfigSnippets.deleteNotificationConfig(GetOrganizationId(), configId);
-        }
+    [Fact]
+    public void GetNotificationConfig_ShouldReturnConfig()
+    {
+        String configId = "csharp-get-config-test111";
+        CreateNotificationConfigSnippet.createNotificationConfig(GetOrganizationId(), configId, GetProjectId(), GetTopic());
+        Assert.NotNull(GetNotificationConfigSnippets.getNotificationConfig(GetOrganizationId(), configId));
+        DeleteNotificationConfigSnippets.deleteNotificationConfig(GetOrganizationId(), configId);
+    }
 
-        [Fact]
-        public void UpdateNotificationConfig_ShouldUpdateConfig()
-        {
-            String configId = "csharp-update-config-test111";
-            CreateNotificationConfigSnippet.createNotificationConfig(GetOrganizationId(), configId, GetProjectId(), GetTopic());
-            Assert.NotNull(UpdateNotificationConfigSnippets.updateNotificationConfig(GetOrganizationId(), configId, GetProjectId(), GetTopic()));
-            DeleteNotificationConfigSnippets.deleteNotificationConfig(GetOrganizationId(), configId);
-        }
+    [Fact]
+    public void UpdateNotificationConfig_ShouldUpdateConfig()
+    {
+        String configId = "csharp-update-config-test111";
+        CreateNotificationConfigSnippet.createNotificationConfig(GetOrganizationId(), configId, GetProjectId(), GetTopic());
+        Assert.NotNull(UpdateNotificationConfigSnippets.updateNotificationConfig(GetOrganizationId(), configId, GetProjectId(), GetTopic()));
+        DeleteNotificationConfigSnippets.deleteNotificationConfig(GetOrganizationId(), configId);
+    }
 
-        private String GetOrganizationId() {
-            return "1081635000895";
-        }
+    private String GetOrganizationId() {
+        return "1081635000895";
+    }
 
-        private String GetProjectId() {
-            return "project-a-id";
-        }
+    private String GetProjectId() {
+        return "project-a-id";
+    }
 
-        private String GetTopic() {
-            return "notifications-sample-topic";
-        }
+    private String GetTopic() {
+        return "notifications-sample-topic";
     }
 }
