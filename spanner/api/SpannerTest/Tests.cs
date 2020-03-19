3575a0d523d6121298cd02f447a682857df21a7e
Shanika Kuruppu <skuruppu@google.com>
2020-03-19 11:53:35 +1100

    Fixed test setup.

--- spanner/api/SpannerTest/Tests.cs
+++ spanner/api/SpannerTest/Tests.cs
@@ -561,19 +561,24 @@ namespace GoogleCloudSamples.Spanner
         [Fact]
         void TestQueryOptions()
         {
+            _spannerCmd.Run("createTableWithDatatypes",
+                _fixture.ProjectId, _fixture.InstanceId, _fixture.DatabaseId);
+            _spannerCmd.Run("writeDatatypesData",
+                _fixture.ProjectId, _fixture.InstanceId, _fixture.DatabaseId);
+
             ConsoleOutput readOutput = _spannerCmd.Run("createConnectionWithQueryOptions",
                 _fixture.ProjectId, _fixture.InstanceId, _fixture.DatabaseId);
             Assert.Equal(0, readOutput.ExitCode);
-            Assert.Contains("4 Venue 4", readOutput.Stdout);
-            Assert.Contains("19 Venue 19", readOutput.Stdout);
-            Assert.Contains("42 Venue 42", readOutput.Stdout);
+            Assert.Contains("VenueId : 4 VenueName : Venue 4", readOutput.Stdout);
+            Assert.Contains("VenueId : 19 VenueName : Venue 19", readOutput.Stdout);
+            Assert.Contains("VenueId : 42 VenueName : Venue 42", readOutput.Stdout);
 
             readOutput = _spannerCmd.Run("queryDataWithQueryOptions",
                 _fixture.ProjectId, _fixture.InstanceId, _fixture.DatabaseId);
             Assert.Equal(0, readOutput.ExitCode);
-            Assert.Contains("4 Venue 4", readOutput.Stdout);
-            Assert.Contains("19 Venue 19", readOutput.Stdout);
-            Assert.Contains("42 Venue 42", readOutput.Stdout);
+            Assert.Contains("VenueId : 4 VenueName : Venue 4", readOutput.Stdout);
+            Assert.Contains("VenueId : 19 VenueName : Venue 19", readOutput.Stdout);
+            Assert.Contains("VenueId : 42 VenueName : Venue 42", readOutput.Stdout);
         }
 
         /// <summary>
