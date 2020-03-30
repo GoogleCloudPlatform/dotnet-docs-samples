968682deb634dafc51658aa060483b2ee959cc95
Amanda Tarafa Mas <atarafamas@google.com>
2020-03-31 00:44:48 +0100

    Jobs V4 dependency update and cleanup.

--- jobs/api/v4/JobsSample/ListCompaniesSample.cs
+++ jobs/api/v4/JobsSample/ListCompaniesSample.cs
@@ -23,11 +23,11 @@ namespace GoogleCloudSamples
         public static object ListCompanies(string projectId, string tenantId)
         {
             CompanyServiceClient companyServiceClient = CompanyServiceClient.Create();
-            TenantName tenantName = new TenantName(projectId, tenantId);
-            TenantOrProjectNameOneof parent = TenantOrProjectNameOneof.From(tenantName);
+            TenantName tenantName = TenantName.FromProjectTenant(projectId, tenantId);
+
             ListCompaniesRequest request = new ListCompaniesRequest
             {
-                ParentAsTenantOrProjectNameOneof = parent
+                ParentAsTenantName = tenantName
             };
             var companies = companyServiceClient.ListCompanies(request);
             foreach (var company in companies)
