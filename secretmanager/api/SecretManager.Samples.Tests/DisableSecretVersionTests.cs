0344766808a2d579454a39781aa7c0c03b2f9c06
Seth Vargo <seth@sethvargo.com>
2020-04-21 17:24:43 -0400

    Update Secret Manager samples to be more canonical

---
+++ secretmanager/api/SecretManager.Samples.Tests/DisableSecretVersionTests.cs
@@ -0,0 +1,40 @@
+/*
+ * Copyright 2020 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+using Xunit;
+using Google.Cloud.SecretManager.V1;
+
+[Collection(nameof(SecretManagerFixture))]
+public class DisableSecretVersionTests
+{
+    private readonly SecretManagerFixture _fixture;
+    private readonly DisableSecretVersionSample _sample;
+
+    public DisableSecretVersionTests(SecretManagerFixture fixture)
+    {
+        _fixture = fixture;
+        _sample = new DisableSecretVersionSample();
+    }
+
+    [Fact]
+    public void DisablesSecretVersions()
+    {
+        SecretVersionName secretVersionName = _fixture.SecretVersionToDisable.SecretVersionName;
+        SecretVersion secretVersion = _sample.DisableSecretVersion(
+          projectId: secretVersionName.ProjectId, secretId: secretVersionName.SecretId, secretVersionId: secretVersionName.SecretVersionId);
+        Assert.Equal(SecretVersion.Types.State.Disabled, secretVersion.State);
+    }
+}
