cf487fe0ca8b81d0f348e61ee206f8f7fa1d0709
Munkhbayar Ganbold <munkhbayarg@google.com>
2020-04-22 15:20:24 -0700

    AutoML:Dataset CRUD

--- automl/api/Snippets/Program.cs
+++ automl/api/Snippets/Program.cs
@@ -33,79 +33,22 @@ namespace GoogleCloudSamples
         public string DisplayName { get; set; }
     }
 
-    [Verb("create_model", HelpText = "Create a new custom model")]
-    public class CreateModelOptions
-    {
-        [Value(0, HelpText = "Your project ID")]
-        public string ProjectID { get; set; }
-
-        [Value(1, HelpText = "Display name for your model")]
-        public string DisplayName { get; set; }
-
-        [Value(2, HelpText = "Name of the dataset to use for training your model")]
-        public string DatasetID { get; set; }
-    }
-
-    [Verb("get_model", HelpText = "Retrieve a model for AutoML")]
-    public class GetModelOptions
-    {
-        [Value(0, HelpText = "Your project ID")]
-        public string ProjectID { get; set; }
-
-        [Value(1, HelpText = "The ID of model to retrieve.")]
-        public string ModelId { get; set; }
-    }
-
-    [Verb("predict", HelpText = "Make a prediction with a custom model")]
-    public class PredictOptions
-    {
-        [Value(0, HelpText = "Your project ID")]
-        public string ProjectID { get; set; }
-
-        [Value(1, HelpText = "ID of the model to use for prediction")]
-        public string ModelID { get; set; }
-    }
-
     public class AutoMLProgram
     {
         public static int Main(string[] args)
         {
             var verbMap = new VerbMap<object>();
-            AutoMLBatchPredict.RegisterCommands(verbMap);
-            AutoMLListModels.RegisterCommands(verbMap);
-            AutoMLDeleteModel.RegisterCommands(verbMap);
             AutoMLDeleteDataset.RegisterCommands(verbMap);
-            AutoMLDeployModel.RegisterCommands(verbMap);
             AutoMLLanguageSentimentAnalysisCreateDataset.RegisterCommands(verbMap);
-            AutoMLLanguageSentimentAnalysisCreateModel.RegisterCommands(verbMap);
-            AutoMLLanguageSentimentAnalysisPredict.RegisterCommands(verbMap);
             AutoMLLanguageTextClassificationCreateDataset.RegisterCommands(verbMap);
-            AutoMLLanguageTextClassificationCreateModel.RegisterCommands(verbMap);
-            AutoMLLanguageTextClassificationPredict.RegisterCommands(verbMap);
             AutoMLLanguageEntityExtractionCreateDataset.RegisterCommands(verbMap);
-            AutoMLLanguageEntityExtractionCreateModel.RegisterCommands(verbMap);
-            AutoMLLanguageEntityExtractionPredict.RegisterCommands(verbMap);
             AutoMLListDatasets.RegisterCommands(verbMap);
-            AutoMLListModelEvaluations.RegisterCommands(verbMap);
-            AutoMLListOperationStatus.RegisterCommands(verbMap);
             AutoMLExportDataset.RegisterCommands(verbMap);
             AutoMLGetDataset.RegisterCommands(verbMap);
-            AutoMLGetOperationStatus.RegisterCommands(verbMap);
-            AutoMLGetModelEvaluation.RegisterCommands(verbMap);
-            AutoMLGetModel.RegisterCommands(verbMap);
             AutoMLImportDataset.RegisterCommands(verbMap);
             AutoMLTranslationCreateDataset.RegisterCommands(verbMap);
-            AutoMLTranslationCreateModel.RegisterCommands(verbMap);
-            AutoMLTranslationPredict.RegisterCommands(verbMap);
-            AutoMLUndeployModel.RegisterCommands(verbMap);
-            AutoMLVisionClassificationDeployModelNodeCount.RegisterCommands(verbMap);
             AutoMLVisionClassificationCreateDataset.RegisterCommands(verbMap);
-            AutoMLVisionClassificationCreateModel.RegisterCommands(verbMap);
-            AutoMLVisionClassificationPredict.RegisterCommands(verbMap);
             AutoMLVisionObjectDetectionCreateDataset.RegisterCommands(verbMap);
-            AutoMLVisionObjectDetectionCreateModel.RegisterCommands(verbMap);
-            AutoMLVisionObjectDetectionPredict.RegisterCommands(verbMap);
-            AutoMLVisionObjectDetectionDeployModelNodeCount.RegisterCommands(verbMap);
 
             verbMap.NotParsedFunc = (err) => 1;
             return (int)verbMap.Run(args);
