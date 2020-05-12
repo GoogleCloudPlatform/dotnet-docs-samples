a0b3d8f804f1b824dfda3476d2f6b936790bceb5
Hugo Sanchez <sanchezhugo.1994@gmail.com>
2020-05-12 13:05:41 -0700

    Adds SecurityCenter code samples

---
+++ securitycenter/api/src/DeleteNotificationConfigSnippets.g.cs
@@ -0,0 +1,35 @@
+/*
+ * Copyright 2020 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+// [START scc_get_notification_config]
+
+using Google.Cloud.SecurityCenter.V1;
+using System;
+
+/// <summary>Snippet for DeleteNotificationConfig</summary>
+public class DeleteNotificationConfigSnippets
+{
+    public static bool DeleteNotificationConfig(string organizationId, string notificationConfigId)
+    {
+        NotificationConfigName notificationConfigName = new NotificationConfigName(organizationId, notificationConfigId);
+        SecurityCenterClient client = SecurityCenterClient.Create();
+
+        client.DeleteNotificationConfig(notificationConfigName);
+        Console.WriteLine($"Deleted Notification config: {notificationConfigName}");
+        return true;
+    }
+}
+// [END] scc_delete_notification_config]
