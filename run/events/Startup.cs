3ff2e13e0a5a02a127c6fffed8cb2029e90ccd8f
Mete Atamel <atamel@google.com>
2020-05-26 15:33:59 +0100

    Added Cloud Run Events C# samples

---
+++ run/events/Startup.cs
@@ -0,0 +1,54 @@
+// Copyright 2020 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+using System;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace HelloEvents
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseRouting();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapGet("/", async context =>
+                {
+                    var target = Environment.GetEnvironmentVariable("TARGET") ?? "World";
+                    await context.Response.WriteAsync($"Hello {target}!\n");
+                });
+
+                endpoints.MapPost("/", async context =>
+                {
+                    var source = context.Request.Headers["ce-source"];
+                    await context.Response.WriteAsync($"source: {source}\n");
+                });
+            });
+        }
+    }
+}
