5cc919b881b068995f0faffb8e4a54ca41652fee
pheonixblade9 <cody.herring@gmail.com>
2020-06-12 14:15:28 -0700

    Add InspectStringWithExclusionDict and tests (#1094)
    * Add InspectStringWithExclusionDict and tests
    
    * Fix tag
    
    * Update InspectStringWithExclusionDictTests.cs
    
    * Update InspectStringWithExclusionDict.cs
    
    Co-authored-by: Cody Herring <codedude@google.com>

--- dlp/api/Snippets.Tests/InspectStringWithExclusionDictTests.cs
+++ dlp/api/Snippets.Tests/InspectStringWithExclusionDictTests.cs
@@ -12,26 +12,27 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+using System.Collections.Generic;
 using Xunit;
 
 namespace GoogleCloudSamples
 {
-    public class InspectStringWithoutOverlapTests : IClassFixture<DlpTestFixture>
+    public class InspectStringWithExclusionDictTests : IClassFixture<DlpTestFixture>
     {
         private DlpTestFixture Fixture { get; }
-        public InspectStringWithoutOverlapTests(DlpTestFixture fixture)
+        public InspectStringWithExclusionDictTests(DlpTestFixture fixture)
         {
             Fixture = fixture;
         }
 
         [Fact]
-        public void TestInspectStringWithoutOverlap()
+        public void TestInspectStringExclusionDict()
         {
-            string testData = "example.com is a domain, james@example.org is an email.";
-            var response = InspectStringWithoutOverlap.Inspect(Fixture.ProjectId, testData);
+            string testData = "Some email addresses: gary@example.com, example@example.com";
+            var response = InspectStringWithExclusionDict.Inspect(Fixture.ProjectId, testData, new List<string> { "example@example.com" });
 
-            Assert.Contains(response.Result.Findings, f => f.Quote.Contains("example.com"));
-            Assert.DoesNotContain(response.Result.Findings, f => f.Quote.Contains("example.org"));
+            Assert.Contains(response.Result.Findings, f => f.Quote.Contains("gary@example.com"));
+            Assert.DoesNotContain(response.Result.Findings, f => f.Quote.Contains("example@example.com"));
         }
     }
 }
