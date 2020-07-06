1c819a3116e5eba7ed977cae8abdfbdabe234490
Jon Skeet <skeet@pobox.com>
2020-07-06 16:50:12 +0100

    Fix runTests

--- functions/helloworld/runTests.ps1
+++ functions/helloworld/runTests.ps1
@@ -12,6 +12,4 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-Import-Module ..\..\..\BuildTools.psm1 -DisableNameChecking
-
 dotnet test --test-adapter-path:. --logger:junit
