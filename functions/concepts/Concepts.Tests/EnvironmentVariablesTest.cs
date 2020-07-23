94ca981ce87ba52cb5214472d28d7ae64398d991
Jon Skeet <skeet@pobox.com>
2020-07-23 17:44:04 +0100

    Add sample for environment variables

---
+++ functions/concepts/Concepts.Tests/EnvironmentVariablesTest.cs
@@ -0,0 +1,53 @@
+﻿// Copyright 2020 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+using System;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Concepts.Tests
+{
+    // Note: these tests modify the "FOO" environment variable
+    // without any attempt to restore the original value. We assume
+    // no other tests will use that environment variable.
+    public class EnvironmentVariablesTest : FunctionTestBase<EnvironmentVariables.Function>
+    {
+        [Fact]
+        public async Task VariableSet()
+        {
+            Environment.SetEnvironmentVariable("FOO", "Test foo value");
+            using (var client = Server.CreateClient())
+            {
+                var response = await client.GetAsync("uri");
+                response.EnsureSuccessStatusCode();
+                var responseBody = await response.Content.ReadAsStringAsync();
+                Assert.Equal("Test foo value", responseBody);
+            }
+
+        }
+
+        [Fact]
+        public async Task VariableNotSet()
+        {
+            Environment.SetEnvironmentVariable("FOO", null);
+            using (var client = Server.CreateClient())
+            {
+                var response = await client.GetAsync("uri");
+                response.EnsureSuccessStatusCode();
+                var responseBody = await response.Content.ReadAsStringAsync();
+                Assert.Equal("Specified environment variable is not set.", responseBody);
+            }
+        }
+    }
+}
