b5c3b3e7bdc409407c9edb7e25a00474a8944225
Lalji Kanjareeya <lalji.kanjareeya@qlogic.io>
2020-09-03 16:07:25 +0530

    added samples

---
+++ datastore/api/TaskList/GetIndex.cs
@@ -0,0 +1,47 @@
+﻿// Copyright 2020 Google Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// [START datastore_admin_index_get]
+
+using Google.Cloud.Datastore.Admin.V1;
+using System;
+
+public class GetIndexSample
+{
+    public Index GetIndex(string projectId, string indexId)
+    {
+        // Create client
+        DatastoreAdminClient datastoreAdminClient = DatastoreAdminClient.Create();
+
+        // Initialize request argument(s)
+        GetIndexRequest getIndexRequest = new GetIndexRequest
+        {
+            ProjectId = projectId,
+            IndexId = indexId
+        };
+
+        Index index = datastoreAdminClient.GetIndex(getIndexRequest);
+
+        Console.WriteLine($"Index Id: {index.IndexId}");
+        Console.WriteLine($"Kind: {index.Kind}");
+        Console.WriteLine("Properties:");
+        foreach (var property in index.Properties)
+        {
+            Console.WriteLine($"Property: {property.Name}");
+            Console.WriteLine($"Direction: {property.Direction}");
+        }
+        return index;
+    }
+}
+// [END datastore_admin_index_get]
