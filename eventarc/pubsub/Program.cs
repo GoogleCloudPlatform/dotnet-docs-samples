bd11613ca363852686fd14832e18e1def63b7ff1
Mete Atamel <atamel@google.com>
2020-09-29 10:06:03 +0100

    Moved run/events-* to eventarc/ and other minor changes to match the Node.js repo. Fixes #1195 (#1196)

--- eventarc/pubsub/Program.cs
+++ eventarc/pubsub/Program.cs
@@ -18,7 +18,7 @@ using Microsoft.Extensions.Hosting;
 
 public class Program
 {
-    // [START run_events_gcs_server]
+    // [START eventarc_pubsub_server]
     public static void Main(string[] args)
     {
         CreateHostBuilder(args).Build().Run();
@@ -34,5 +34,5 @@ public class Program
                 webBuilder.UseStartup<Startup>().UseUrls(url);
             });
     }
-    // [END run_events_gcs_server]
+    // [END eventarc_pubsub_server]
 }
