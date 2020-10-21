ab4c29b93e5ab70a54def4217f1d888e46a098b4
Lalji Kanjareeya <lalji.kanjareeya@qlogic.io>
2020-10-21 11:45:42 +0530

    created database separate connection string for database and table creation

--- spanner/api/Spanner.Samples/CreateDatabaseAsync.cs
+++ spanner/api/Spanner.Samples/CreateDatabaseAsync.cs
@@ -23,36 +23,41 @@ public class CreateDatabaseAsyncSample
     {
         string connectionString = $"Data Source=projects/{projectId}/instances/{instanceId}";
 
-        using var connection = new SpannerConnection(connectionString);
-        string createStatement = $"CREATE DATABASE `{databaseId}`";
-        using var createDbCommand = connection.CreateDdlCommand(createStatement);
-        await createDbCommand.ExecuteNonQueryAsync();
+        using (var connection = new SpannerConnection(connectionString))
+        {
+            string createStatement = $"CREATE DATABASE `{databaseId}`";
+            using var createDbCommand = connection.CreateDdlCommand(createStatement);
+            await createDbCommand.ExecuteNonQueryAsync();
+        }
 
         // Update connection string with Database ID for table creation.
         connectionString += $"/databases/{databaseId}";
-        // Define create table statement for table #1.
-        string createTableStatement =
-        @"CREATE TABLE Singers (
+        using (var connection = new SpannerConnection(connectionString))
+        {
+            // Define create table statement for table #1.
+            string createTableStatement =
+            @"CREATE TABLE Singers (
                      SingerId INT64 NOT NULL,
                      FirstName STRING(1024),
                      LastName STRING(1024),
                      ComposerInfo BYTES(MAX)
                  ) PRIMARY KEY (SingerId)";
 
-        using var createTableCommand = connection.CreateDdlCommand(createTableStatement);
-        await createTableCommand.ExecuteNonQueryAsync();
+            using var createTableCommand = connection.CreateDdlCommand(createTableStatement);
+            await createTableCommand.ExecuteNonQueryAsync();
 
-        // Define create table statement for table #2.
-        createTableStatement =
-        @"CREATE TABLE Albums (
+            // Define create table statement for table #2.
+            createTableStatement =
+            @"CREATE TABLE Albums (
                      SingerId INT64 NOT NULL,
                      AlbumId INT64 NOT NULL,
                      AlbumTitle STRING(MAX)
                  ) PRIMARY KEY (SingerId, AlbumId),
                  INTERLEAVE IN PARENT Singers ON DELETE CASCADE";
 
-        using var createTableCommand1 = connection.CreateDdlCommand(createTableStatement);
-        await createTableCommand1.ExecuteNonQueryAsync();
+            using var createTableCommand1 = connection.CreateDdlCommand(createTableStatement);
+            await createTableCommand1.ExecuteNonQueryAsync();
+        }
     }
 }
 // [END spanner_create_database]
