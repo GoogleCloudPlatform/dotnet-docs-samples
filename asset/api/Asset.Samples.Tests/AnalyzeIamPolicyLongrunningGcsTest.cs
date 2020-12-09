6d534b6c9ece59f0cc205873f5b74725275d528d
Donghe Zhao <donghez@google.com>
2020-12-09 18:24:05 +0000

    reverting GcsTest because of copy paste error

--- asset/api/Asset.Samples.Tests/AnalyzeIamPolicyLongrunningGcsTest.cs
+++ asset/api/Asset.Samples.Tests/AnalyzeIamPolicyLongrunningGcsTest.cs
@@ -1,63 +1,46 @@
-// Copyright(c) 2020 Google LLC.
-//
-// Licensed under the Apache License, Version 2.0 (the "License"); you may not
-// use this file except in compliance with the License. You may obtain a copy of
-// the License at
-//
-// http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
-// WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
-// License for the specific language governing permissions and limitations under
-// the License.
-//
-// [START asset_quickstart_analyze_iam_policy_longrunning_gcs]
+/*
+ * Copyright (c) 2020 Google LLC.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
 
-using Google.Api.Gax;
 using Google.Cloud.Asset.V1;
-using System.Collections.Generic;
-using System.Linq;
+using System;
+using Xunit;
 
-public class AnalyzeIamPolicyLongrunningGcsSample
+[Collection(nameof(AssetFixture))]
+public class AnalyzeIamPolicyLongrunningGcsTest
 {
-    public AnalyzeIamPolicyLongrunningRequest AnalyzeIamPolicyLongrunning(
-      string scope, string fullResourceName, string uri)
+    private readonly AssetFixture _fixture;
+    private readonly AnalyzeIamPolicyLongrunningGcsSample _sample;
+
+    public AnalyzeIamPolicyLongrunningGcsTest(AssetFixture fixture)
     {
-        // Create the client.
-        AssetServiceClient client = AssetServiceClient.Create();
+        _fixture = fixture;
+        _sample = new AnalyzeIamPolicyLongrunningGcsSample();
+    }
 
-        // Build the request.
-        AnalyzeIamPolicyLongrunningRequest request = new AnalyzeIamPolicyLongrunningRequest
-        {
-            AnalysisQuery = new IamPolicyAnalysisQuery
-            {
-                Scope = scope,
-                ResourceSelector = new IamPolicyAnalysisQuery.Types.ResourceSelector
-                {
-                    FullResourceName = fullResourceName,
-                },
-                Options = new IamPolicyAnalysisQuery.Types.Options
-                {
-                    ExpandGroups = true,
-                    OutputGroupEdges = true,
-                },
-            },
-            OutputConfig = new IamPolicyAnalysisOutputConfig
-            {
-                GcsDestination = new IamPolicyAnalysisOutputConfig.Types.GcsDestination
-                {
-                    Uri = uri,
-                },
-            },
-        };
+    [Fact]
+    public void TestAnalyzeIamPolicyLongrunningGcs()
+    {
+        // Run the sample code.
+        string scope = $"projects/{_fixture.ProjectId}";
+        string fullResourceName =
+            $"//cloudresourcemanager.googleapis.com/projects/{_fixture.ProjectId}";
+        string uri = $"gs://{_fixture.BucketName}/my-analysis.json";
+        AnalyzeIamPolicyLongrunningRequest returnedRequest =
+            _sample.AnalyzeIamPolicyLongrunning(scope, fullResourceName, uri);
 
-        // Start the analyze long-running operation
-        var operation = client.AnalyzeIamPolicyLongrunning(request);
-        // Wait for it to complete
-        operation = operation.PollUntilCompleted();
-        // Return the metadata(request)
-        return operation.Metadata;
+        Assert.Equal(uri, returnedRequest.OutputConfig.GcsDestination.Uri);
     }
 }
-// [END asset_quickstart_analyze_iam_policy_longrunning_gcs]
