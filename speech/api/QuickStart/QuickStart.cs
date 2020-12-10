1187778d051ef3ec13989a2481d3fbd35ffe005b
b-loved-dreamer <franklinsnunez@gmail.com>
2020-12-10 12:51:31 -0800

    refactor: quickstart now uses a gcs uri

--- speech/api/QuickStart/QuickStart.cs
+++ speech/api/QuickStart/QuickStart.cs
@@ -24,7 +24,7 @@ namespace GoogleCloudSamples
     public class QuickStart
     {
         // The name of the local audio file to transcribe
-        public static string DEMO_FILE = "audio.raw";
+        public static string GCS_URI = "gs://cloud-samples-data/speech/brooklyn_bridge.raw";
         public static void Main(string[] args)
         {
             var speech = SpeechClient.Create();
@@ -33,7 +33,7 @@ namespace GoogleCloudSamples
                 Encoding = RecognitionConfig.Types.AudioEncoding.Linear16,
                 SampleRateHertz = 16000,
                 LanguageCode = "en",
-            }, RecognitionAudio.FromFile(DEMO_FILE));
+            }, RecognitionAudio.FromStorageUri(GCS_URI));
             foreach (var result in response.Results)
             {
                 foreach (var alternative in result.Alternatives)
