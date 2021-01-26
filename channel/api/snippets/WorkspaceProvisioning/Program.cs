9b3762d1dc846f8ab212dedad7c3becfc0a77280
Danny Ackerman <danny.ackerman@gmail.com>
2021-01-26 09:54:32 -0500

    Refactored with suggested edits

--- channel/api/snippets/WorkspaceProvisioning/Program.cs
+++ channel/api/snippets/WorkspaceProvisioning/Program.cs
@@ -1,3 +1,19 @@
+/*
+ * Copyright (c) 2021 Google Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
 using Google.Apis.Auth.OAuth2;
 using Google.Api.Gax;
 using Google.Cloud.Channel.V1;
@@ -10,14 +26,16 @@ namespace CodeLab
 {
     class Program
     {
+        // [START credentials]
+
         /***************** REPLACE WITH YOUR OWN VALUES ********************************/
-        private static readonly String jsonPrivateKeyFile = "path/to/json_key_file.json";
-        private static readonly String resellerAdminUser = "admin@yourresellerdomain.com";
-        private static readonly String accountId = "C012345";
-        private static readonly String customerDomain = "example.com";
+        private static readonly string jsonPrivateKeyFile = "path/to/json_key_file.json";
+        private static readonly string resellerAdminUser = "admin@yourresellerdomain.com";
+        private static readonly string accountId = "C012345";
+        private static readonly string customerDomain = "example.com";
         /*******************************************************************************/
 
-        private static readonly String accountParent = "accounts/" + accountId;
+        private static readonly string accountParent = "accounts/" + accountId;
 
         private static CloudChannelServiceClient cloudChannelServiceClient;
 
@@ -29,7 +47,7 @@ namespace CodeLab
             {
                 credential =
                     GoogleCredential.FromStream(stream)
-                        .CreateScoped(new string[] {"https://www.googleapis.com/auth/apps.order"})
+                        .CreateScoped(CloudChannelServiceClient.DefaultScopes)
                         .CreateWithUser(resellerAdminUser);
             }
 
@@ -39,32 +57,28 @@ namespace CodeLab
                         TokenAccessMethod = ((ITokenAccess) credential).GetAccessTokenForRequestAsync
                     }
                     .Build();
+            // [END credentials]
 
+            // [START pickOffer]
             // For the purpose of this codelab, the code lists all offers and picks
             // the first offer for Google Workspace Business Standard on an Annual
             // plan. This is needed because offerIds vary from one account to another,
             // but this is not a recommended model for your production integration
             PagedEnumerable<ListOffersResponse, Offer> listOffersResponse =
-                client.ListOffers(new ListOffersRequest 
+                client.ListOffers(new ListOffersRequest
                 {
                     Parent = accountParent
                 });
-            
-            Offer selectedOffer = new Offer();
-            String sampleOffer = "Google Workspace Business Standard";
-            String samplePlan = "Commitment";
 
-            foreach (Offer offer in listOffersResponse.ToList())
-            {
-                String offerName = offer.Sku.MarketingInfo.DisplayName;
-                String offerPlan = offer.Plan.PaymentPlan.ToString();
-                if (offerName == sampleOffer && offerPlan == samplePlan)
-                {
-                    selectedOffer = offer;
-                    break;
-                }
-            }
+            string sampleOffer = "Google Workspace Business Standard";
+            PaymentPlan samplePlan = PaymentPlan.Commitment;
 
+            Offer selectedOffer = listOffersResponse.FirstOrDefault(o =>
+                o.Sku.MarketingInfo.DisplayName == sampleOffer && o.Plan.PaymentPlan == samplePlan);
+            // [END pickOffer]
+
+            // [START checkExists]
+            // Determine if customer already has a cloud identity
             CheckCloudIdentityAccountsExistRequest checkCloudIdentityAccountsExistRequest =
                 new CheckCloudIdentityAccountsExistRequest
                 {
@@ -80,38 +94,48 @@ namespace CodeLab
                 throw new Exception(
                     "Cloud identity already exists. Customer must be transferred. Out of scope for this codelab");
             }
+            // [END checkExists]
 
+            // [START createCustomer]
+            // Create the Customer resource
             PostalAddress postalAddress = new PostalAddress
             {
-                AddressLines = {
+                AddressLines =
+                {
                     "123 Main St"
                 },
-                PostalCode = "97224", RegionCode = "US"
+                PostalCode = "97224",
+                RegionCode = "US"
             };
 
             Customer customer = new Customer
             {
-                OrgDisplayName = "Acme Corp", OrgPostalAddress = postalAddress, Domain = customerDomain
+                OrgDisplayName = "Acme Corp",
+                OrgPostalAddress = postalAddress,
+                Domain = customerDomain
             };
 
             CreateCustomerRequest createCustomerRequest =
-                new CreateCustomerRequest {
-                    Parent = accountParent, 
+                new CreateCustomerRequest
+                {
+                    Parent = accountParent,
                     Customer = customer
                 };
 
             Customer createCustomerResponse = client.CreateCustomer(createCustomerRequest);
+            // [END createCustomer]
 
-            CloudIdentityInfo cloudIdentityInfo =
-                new CloudIdentityInfo
-                {
-                    AlternateEmail = "marty.mcfly@gmail.com",
-                    LanguageCode = "en-US"
-                };
+            // [START provisionCloudIdentity]
+            CloudIdentityInfo cloudIdentityInfo = new CloudIdentityInfo
+            {
+                AlternateEmail = "marty.mcfly@gmail.com",
+                LanguageCode = "en-US"
+            };
 
             AdminUser adminUser = new AdminUser
             {
-                GivenName = "Marty", FamilyName = "McFly",
+                GivenName = "Marty",
+                FamilyName = "McFly",
                 Email = "admin@" + createCustomerResponse.Domain
             };
 
@@ -125,31 +149,36 @@ namespace CodeLab
 
             var provisionCloudIdentityOp = client.ProvisionCloudIdentity(provisionCloudIdentityRequest);
 
-            PollSettings pollSettings = new PollSettings(Expiration.FromTimeout(TimeSpan.FromSeconds(30)),
-                TimeSpan.FromSeconds(1));
+            provisionCloudIdentityOp.PollUntilCompleted();
+            // [END provisionCloudIdentity]
 
-            provisionCloudIdentityOp.PollUntilCompleted(pollSettings);
-
-            RenewalSettings renewalSettings =
-                new RenewalSettings
+            // [START createEntitlement]
+            RenewalSettings renewalSettings = new RenewalSettings
+            {
+                EnableRenewal = true,
+                PaymentPlan = PaymentPlan.Commitment,
+                PaymentCycle = new Period
                 {
-                    EnableRenewal = true, PaymentPlan = PaymentPlan.Commitment,
-                    PaymentCycle =
-                        new Period {PeriodType = PeriodType.Year, Duration = 1}
-                };
+                    PeriodType = PeriodType.Year,
+                    Duration = 1
+                }
+            };
 
-            CommitmentSettings commitmentSettings =
-                new CommitmentSettings {
-                    RenewalSettings = renewalSettings
-                };
+            CommitmentSettings commitmentSettings = new CommitmentSettings
+            {
+                RenewalSettings = renewalSettings
+            };
 
             Entitlement entitlement = new Entitlement
             {
                 Offer = selectedOffer.Name,
-                Parameters = {
-                    new Parameter {
-                        Name = "num_units", 
-                        Value = new Value {
+                Parameters =
+                {
+                    new Parameter
+                    {
+                        Name = "num_units",
+                        Value = new Value
+                        {
                             Int64Value = 5
                         }
                     }
@@ -166,7 +195,8 @@ namespace CodeLab
 
             var createEntitlementOp = client.CreateEntitlement(createEntitlementRequest);
 
-            createEntitlementOp.PollUntilCompleted(pollSettings);
+            createEntitlementOp.PollUntilCompleted();
+            // [END createEntitlement]
 
             Console.WriteLine("Successfully created customer " + customerDomain);
         }
