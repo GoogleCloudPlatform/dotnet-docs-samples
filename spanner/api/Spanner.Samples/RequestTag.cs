98c9b75073870005a42d0268990ab0aabca9bb07
Knut Olav Løite <koloite@gmail.com>
2021-08-03 21:05:00 +0200

    feat: add samples for tagging

---
+++ spanner/api/Spanner.Samples/RequestTag.cs
@@ -0,0 +1,58 @@
+﻿// Copyright 2021 Google Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// [START spanner_set_request_tag]
+
+using Google.Cloud.Spanner.Data;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+public class RequestTagAsyncSample
+{
+    public class Album
+    {
+        public int SingerId { get; set; }
+        public int AlbumId { get; set; }
+        public string AlbumTitle { get; set; }
+    }
+
+    public async Task<List<Album>> RequestTagAsync(string projectId, string instanceId, string databaseId)
+    {
+        string connectionString = $"Data Source=projects/{projectId}/instances/{instanceId}/databases/{databaseId}";
+
+        using var connection = new SpannerConnection(connectionString);
+        using var cmd = connection.CreateSelectCommand(
+            $"SELECT SingerId, AlbumId, AlbumTitle FROM Albums");
+        // Sets the request tag to "app=concert,env=dev,action=select".
+        // This request tag will only be set on this request.
+        cmd.Tag = "app=concert,env=dev,action=select";
+
+        var albums = new List<Album>();
+        using var reader = await cmd.ExecuteReaderAsync();
+        while (await reader.ReadAsync())
+        {
+            var album = new Album
+            {
+                SingerId = reader.GetFieldValue<int>("SingerId"),
+                AlbumId = reader.GetFieldValue<int>("AlbumId"),
+                AlbumTitle = reader.GetFieldValue<string>("AlbumTitle")
+            };
+            albums.Add(album);
+            Console.WriteLine($"SingerId: {album.SingerId}, AlbumId: {album.AlbumId}, AlbumTitle: {album.AlbumTitle}");
+        }
+        return albums;
+    }
+}
+// [END spanner_set_request_tag]
---
+++ spanner/api/Spanner.Samples/TransactionTagAsync.cs
@@ -0,0 +1,86 @@
+﻿// Copyright 2021 Google Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// [START spanner_set_transaction_tag]
+
+using Google.Cloud.Spanner.Data;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+public class TransactionTagAsyncSample
+{
+    public class Venue
+    {
+        public long VenueId { get; set; }
+        public string VenueName { get; set; }
+        public long Capacity { get; set; }
+    }
+    
+    public async Task<List<Venue>> TransactionTagAsync(string projectId, string instanceId, string databaseId)
+    {
+        string connectionString = $"Data Source=projects/{projectId}/instances/{instanceId}/databases/{databaseId}";
+        using var connection = new SpannerConnection(connectionString);
+        await connection.OpenAsync();
+
+        return await connection.RunWithRetriableTransactionAsync(async transaction =>
+        {
+            var venues = new List<Venue>();
+            // Sets the transaction tag to "app=concert,env=dev".
+            // This transaction tag will be applied to all the individual operations inside
+            // the transaction.
+            transaction.Tag = "app=concert,env=dev";
+            var selectCommand = connection.CreateSelectCommand(
+                @"SELECT VenueId, VenueName, Capacity
+                  FROM Venues
+                  WHERE OutdoorVenue = @outdoorVenue",
+                new SpannerParameterCollection
+                {
+                    new SpannerParameter("outdoorVenue", SpannerDbType.Bool, false)
+                });
+            selectCommand.Transaction = transaction;
+            // Sets the request tag to "app=concert,env=dev,action=select".
+            // This request tag will only be set on this request.
+            selectCommand.Tag = "app=concert,env=dev,action=select";
+            using var reader = await selectCommand.ExecuteReaderAsync();
+            while (await reader.ReadAsync())
+            {
+                var venue = new Venue
+                {
+                    VenueId = reader.GetFieldValue<long>("VenueId"),
+                    VenueName = reader.GetFieldValue<string>("VenueName"),
+                    Capacity = reader.GetFieldValue<long>("Capacity")
+                };
+                venues.Add(venue);
+                // Update the capacity of the venue.
+                venue.Capacity /= 4;
+                var updateCommand = connection.CreateDmlCommand(
+                    "UPDATE Venues SET Capacity = @capacity WHERE VenueId = @venueId",
+                    new SpannerParameterCollection
+                    {
+                        {"capacity", SpannerDbType.Int64, venue.Capacity},
+                        {"venueId", SpannerDbType.Int64, venue.VenueId}
+                    });
+                updateCommand.Transaction = transaction;
+                // Sets the request tag to "app=concert,env=dev,action=update".
+                // This request tag will only be set on this request.
+                updateCommand.Tag = "app=concert,env=dev,action=update";
+                await updateCommand.ExecuteNonQueryAsync();
+                Console.WriteLine($"Capacity of {venue.VenueName} updated to {venue.Capacity}");
+            }
+            return venues;
+        });
+    }
+}
+// [END spanner_set_transaction_tag]
