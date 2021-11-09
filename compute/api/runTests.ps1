a864c02cd45859f5560d9ca1ee73a34e890a1c10
Amanda Tarafa Mas <atarafamas@google.com>
2021-11-09 13:46:17 +0000

    tests(Compute): Load tools from the correct path. This is why timeout was not having any effect.

--- compute/api/runTests.ps1
+++ compute/api/runTests.ps1
@@ -11,7 +11,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-import-module -DisableNameChecking ..\..\..\BuildTools.psm1
+import-module -DisableNameChecking ..\..\BuildTools.psm1
 
 Set-TestTimeout 1200
 
