e0d039c39d4c6b1b29895868716352b37633efd4
JesseLovelace <43148100+JesseLovelace@users.noreply.github.com>
2021-11-16 09:44:28 -0800

    Add transfer service quickstart

---
+++ storagetransfer/api/StorageTransfer.Samples.Tests/QuickstartTest.cs
@@ -0,0 +1,37 @@
+﻿using Google.Cloud.StorageTransfer.V1;
+using Xunit;
+
+namespace StorageTransfer.Samples.Tests
+{
+    [Collection(nameof(StorageFixture))]
+    public class QuickstartTest
+    {
+        private readonly StorageFixture _fixture;
+
+        public QuickstartTest(StorageFixture fixture)
+        {
+            _fixture = fixture;
+        }
+
+        [Fact]
+        public void TestQuickstart()
+        {
+            QuickstartSample quickstartSample = new QuickstartSample();
+            var transferJob = quickstartSample.Quickstart(_fixture.ProjectId, _fixture.BucketNameSource, _fixture.BucketNameSink);
+            Assert.Contains("transferJobs/", transferJob.Name);
+
+            // Delete the job to clean up
+            _fixture.Sts.UpdateTransferJob(new UpdateTransferJobRequest()
+            {
+                ProjectId = _fixture.ProjectId,
+                JobName = transferJob.Name,
+                TransferJob = new TransferJob()
+                {
+                    Name = transferJob.Name,
+                    Status = TransferJob.Types.Status.Deleted
+                }
+            });
+        }
+    }
+}
+
---
+++ storagetransfer/api/StorageTransfer.Samples.Tests/StorageFixture.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using Google.Apis.Storage.v1.Data;
+using Google.Cloud.Storage.V1;
+using Google.Cloud.StorageTransfer.V1;
+using Xunit;
+namespace StorageTransfer.Samples.Tests
+{
+    [CollectionDefinition(nameof(StorageFixture))]
+    public class StorageFixture : IDisposable, ICollectionFixture<StorageFixture>
+    {
+        public string ProjectId { get; }
+        public string BucketNameSource { get; } = Guid.NewGuid().ToString();
+        public string BucketNameSink { get; } = Guid.NewGuid().ToString();
+        public StorageClient Storage { get; } = StorageClient.Create();
+        public StorageTransferServiceClient Sts { get; } = StorageTransferServiceClient.Create();
+
+        public StorageFixture()
+        {
+            ProjectId = Environment.GetEnvironmentVariable("GOOGLE_PROJECT_ID");
+            if (string.IsNullOrWhiteSpace(ProjectId))
+            {
+                throw new Exception("You need to set the Environment variable 'GOOGLE_PROJECT_ID' with your Google Cloud Project's project id.");
+            }
+
+            CreateBucketAndGrantStsPermissions(BucketNameSink);
+            CreateBucketAndGrantStsPermissions(BucketNameSource);
+        }
+
+        private void CreateBucketAndGrantStsPermissions(string bucketName)
+        {
+            var bucket = Storage.CreateBucket(ProjectId, new Bucket
+            {
+                Name = bucketName
+            });
+
+            string email = Sts.GetGoogleServiceAccount(new GetGoogleServiceAccountRequest()
+            {
+                ProjectId = ProjectId
+            }).AccountEmail;
+            string member = "serviceAccount:" + email;
+            string objectViewer = "roles/storage.objectViewer";
+            string bucketReader = "roles/storage.legacyBucketReader";
+            string bucketWriter = "roles/storage.legacyBucketWriter";
+
+            var policy = Storage.GetBucketIamPolicy(bucketName, new GetBucketIamPolicyOptions
+            {
+                RequestedPolicyVersion = 3
+            });
+            // Set the policy schema version. For more information, please refer to https://cloud.google.com/iam/docs/policies#versions.
+            policy.Version = 3;
+
+            Policy.BindingsData objectViewerBinding = new Policy.BindingsData
+            {
+                Role = objectViewer,
+                Members = new List<string> { member }
+            };
+            Policy.BindingsData bucketReaderBinding = new Policy.BindingsData
+            {
+                Role = bucketReader,
+                Members = new List<string> { member }
+            };
+            Policy.BindingsData bucketWriterBinding = new Policy.BindingsData
+            {
+                Role = bucketWriter,
+                Members = new List<string> { member }
+            };
+
+            policy.Bindings.Add(objectViewerBinding);
+            policy.Bindings.Add(bucketReaderBinding);
+            policy.Bindings.Add(bucketWriterBinding);
+
+            Storage.SetBucketIamPolicy(bucketName, policy);
+        }
+
+        public void Dispose()
+        {
+            Storage.DeleteBucket(BucketNameSink);
+            Storage.DeleteBucket(BucketNameSource);
+        }
+    }
+}
