88536b4dca85a2d812b858a9402ba962eb7d0667
JesseLovelace <43148100+JesseLovelace@users.noreply.github.com>
2021-11-18 15:34:32 -0800

    Add copyright and try catches

--- storagetransfer/api/StorageTransfer.Samples.Tests/QuickstartTest.cs
+++ storagetransfer/api/StorageTransfer.Samples.Tests/QuickstartTest.cs
@@ -1,12 +1,30 @@
-﻿using Google.Cloud.StorageTransfer.V1;
+﻿/**
+ * Copyright 2021 Google Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+using System;
+using Google.Cloud.StorageTransfer.V1;
 using Xunit;
 
 namespace StorageTransfer.Samples.Tests
 {
     [Collection(nameof(StorageFixture))]
-    public class QuickstartTest
+    public class QuickstartTest : IDisposable
     {
         private readonly StorageFixture _fixture;
+        private string transferJobName;
 
         public QuickstartTest(StorageFixture fixture)
         {
@@ -19,19 +37,28 @@ namespace StorageTransfer.Samples.Tests
             QuickstartSample quickstartSample = new QuickstartSample();
             var transferJob = quickstartSample.Quickstart(_fixture.ProjectId, _fixture.BucketNameSource, _fixture.BucketNameSink);
             Assert.Contains("transferJobs/", transferJob.Name);
+            transferJobName = transferJob.Name;
+        }
 
-            // Delete the job to clean up
-            _fixture.Sts.UpdateTransferJob(new UpdateTransferJobRequest()
+        public void Dispose()
+        {
+            try
             {
-                ProjectId = _fixture.ProjectId,
-                JobName = transferJob.Name,
-                TransferJob = new TransferJob()
+                _fixture.Sts.UpdateTransferJob(new UpdateTransferJobRequest()
                 {
-                    Name = transferJob.Name,
-                    Status = TransferJob.Types.Status.Deleted
-                }
-            });
+                    ProjectId = _fixture.ProjectId,
+                    JobName = transferJobName,
+                    TransferJob = new TransferJob()
+                    {
+                        Name = transferJobName,
+                        Status = TransferJob.Types.Status.Deleted
+                    }
+                });
+            }
+            catch (Exception)
+            {
+                // Do nothing, we delete on a best effort basis.
+            }
         }
     }
 }
-
