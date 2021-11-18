75e7be232beb1a0f6e50b6f3e77bb5f4c8ec3e35
JesseLovelace <43148100+JesseLovelace@users.noreply.github.com>
2021-11-18 15:34:32 -0800

    Add copyright and try catches

--- storagetransfer/api/StorageTransfer.Samples.Tests/StorageFixture.cs
+++ storagetransfer/api/StorageTransfer.Samples.Tests/StorageFixture.cs
@@ -1,10 +1,25 @@
-﻿using System;
+﻿/**
+ * Copyright 2021 Google Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+using System;
 using System.Collections.Generic;
 using Google.Apis.Storage.v1.Data;
 using Google.Cloud.Storage.V1;
 using Google.Cloud.StorageTransfer.V1;
 using Xunit;
-
 namespace StorageTransfer.Samples.Tests
 {
     [CollectionDefinition(nameof(StorageFixture))]
@@ -76,8 +91,22 @@ namespace StorageTransfer.Samples.Tests
 
         public void Dispose()
         {
-            Storage.DeleteBucket(BucketNameSink);
-            Storage.DeleteBucket(BucketNameSource);
+            try
+            {
+                Storage.DeleteBucket(BucketNameSink);
+            }
+            catch (Exception)
+            {
+                // Do nothing, we delete on a best effort basis.
+            }
+            try
+            {
+                Storage.DeleteBucket(BucketNameSource);
+            }
+            catch (Exception)
+            {
+                // Do nothing, we delete on a best effort basis.
+            }
         }
     }
 }
