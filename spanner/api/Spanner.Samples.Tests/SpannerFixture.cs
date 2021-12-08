04ff4cc0f79750cd6750cb028004f7a9d894a9d3
Amanda Tarafa Mas <atarafamas@google.com>
2021-12-08 16:34:35 +0000

    tests(Spanner): Makes temporary DB names Guid based Instead of timed based, which was provoking clashes between runs on different computers.

--- spanner/api/Spanner.Samples.Tests/SpannerFixture.cs
+++ spanner/api/Spanner.Samples.Tests/SpannerFixture.cs
@@ -536,7 +536,8 @@ public class SpannerFixture : IAsyncLifetime, ICollectionFixture<SpannerFixture>
     public async Task RunWithTemporaryDatabaseAsync(string instanceId, Func<string, Task> testFunction,
         params string[] extraStatements)
     {
-        var databaseId = $"my-db-{DateTimeOffset.UtcNow.ToUnixTimeMilliseconds()}";
+        // For temporary DBs we don't need a time based ID, as we delete them inmediately.
+        var databaseId = $"temp-db-{Guid.NewGuid().ToString("N").Substring(0, 20)}";
         await RunWithTemporaryDatabaseAsync(instanceId, databaseId, testFunction, extraStatements);
     }
     
