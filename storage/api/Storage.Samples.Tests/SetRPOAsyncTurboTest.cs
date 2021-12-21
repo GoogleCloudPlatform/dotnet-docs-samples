6a74a53152852e1b73c0e514e2311be05fefb0ab
anshuldavid <anshuldavid@google.com>
2021-12-21 20:44:47 +0530

    TurboReplication changes

---
+++ storage/api/Storage.Samples.Tests/SetRPOAsyncTurboTest.cs
@@ -0,0 +1,39 @@
+﻿
+// Copyright 2021 Google Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+using Xunit;
+
+[Collection(nameof(StorageFixture))]
+public class SetRPOAsyncTurboTest
+{
+    private readonly StorageFixture _fixture;
+
+    public SetRPOAsyncTurboTest(StorageFixture fixture)
+    {
+        _fixture = fixture;
+    }
+
+    [Fact]
+    public void SetRPOAsyncTurbo()
+    {
+        SetRPOAsyncTurboSample setRPOAsyncTurboSample = new SetRPOAsyncTurboSample();
+        GetBucketMetadataSample getBucketMetadataSample = new GetBucketMetadataSample();
+        var obj = getBucketMetadataSample.GetBucketMetadata(_fixture.BucketNameGeneric);
+        setRPOAsyncTurboSample.SetRPOAsyncTurbo(_fixture.BucketNameGeneric);
+        _fixture.SleepAfterBucketCreateUpdateDelete();
+
+        Assert.Equal("ASYNC_TURBO", obj.Rpo);
+    }
+}
