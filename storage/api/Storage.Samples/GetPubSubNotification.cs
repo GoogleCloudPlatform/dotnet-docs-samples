0304b09451b6cddfffac920419ce2f37e9c79b9d
Anshul David <43073266+anshuldavid13@users.noreply.github.com>
2022-01-27 00:17:33 +0530

    feat(Storage): Pub/Sub notification samples (#1578)

---
+++ storage/api/Storage.Samples/GetPubSubNotification.cs
@@ -0,0 +1,44 @@
+﻿// Copyright 2022 Google Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// [START storage_print_pubsub_bucket_notification]
+
+using Google.Apis.Storage.v1.Data;
+using Google.Cloud.Storage.V1;
+using System;
+
+public class GetPubSubNotificationSample
+{
+    public Notification GetPubSubNotification(
+        string bucketName = "your-unique-bucket-name",
+        string notificationId = "notificationId")
+    {
+        StorageClient storage = StorageClient.Create();
+        Notification notification = storage.GetNotification(bucketName, notificationId);
+
+        Console.WriteLine("ID: " + notification.Id);
+        Console.WriteLine("Topic: " + notification.Topic);
+        Console.WriteLine("EventTypes: " + notification.EventTypes);
+        Console.WriteLine("CustomAttributes: " + notification.CustomAttributes);
+        Console.WriteLine("PayloadFormat: " + notification.PayloadFormat);
+        Console.WriteLine("ObjectNamePrefix: " + notification.ObjectNamePrefix);
+        Console.WriteLine("ETag: " + notification.ETag);
+        Console.WriteLine("SelfLink: " + notification.SelfLink);
+        Console.WriteLine("Kind: " + notification.Kind);
+
+        return notification;
+    }
+}
+
+// [END storage_print_pubsub_bucket_notification]
