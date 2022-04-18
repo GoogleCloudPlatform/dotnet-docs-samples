c85ce93f22b72d3f2426d0f63ecf424d924b4e1b
Jonathan Simon <jsimonweb@gmail.com>
2022-04-18 12:20:58 -0700

    Address review comments.

--- cloud-sql/mysql/ConnectUnix.cs
+++ cloud-sql/mysql/ConnectUnix.cs
@@ -25,17 +25,17 @@ namespace CloudSql
         public static MySqlConnectionStringBuilder NewMysqlUnixSocketConnectionString()
         {
             // Equivalent connection string:
-            // "Server=<dbSocketDir>/<INSTANCE_CONNECTION_NAME>;Uid=<DB_USER>;Pwd=<DB_PASS>;Database=<DB_NAME>;Protocol=unix"
-            String dbSocketDir = Environment.GetEnvironmentVariable("INSTANCE_UNIX_SOCKET") ?? "/cloudsql";
-            String instanceConnectionName = Environment.GetEnvironmentVariable("INSTANCE_CONNECTION_NAME");
+            // "Server=<INSTANCE_UNIX_SOCKET>;Uid=<DB_USER>;Pwd=<DB_PASS>;Database=<DB_NAME>;Protocol=unix"
             var connectionString = new MySqlConnectionStringBuilder()
             {
                 // The Cloud SQL proxy provides encryption between the proxy and instance.
                 SslMode = MySqlSslMode.None,
-                // Remember - storing secrets in plain text is potentially unsafe. Consider using
-                // something like https://cloud.google.com/secret-manager/docs/overview to help keep
-                // secrets secret.
-                Server = String.Format("{0}/{1}", dbSocketDir, instanceConnectionName),
+
+                // Note: Saving credentials in environment variables is convenient, but not
+                // secure - consider a more secure solution such as
+                // Cloud Secret Manager (https://cloud.google.com/secret-manager) to help
+                // keep secrets safe.
+                Server = Environment.GetEnvironmentVariable("INSTANCE_UNIX_SOCKET"), // e.g. '/cloudsql/project:region:instance'
                 UserID = Environment.GetEnvironmentVariable("DB_USER"),   // e.g. 'my-db-user
                 Password = Environment.GetEnvironmentVariable("DB_PASS"), // e.g. 'my-db-password'
                 Database = Environment.GetEnvironmentVariable("DB_NAME"), // e.g. 'my-database'
