c86675c9ac350195843c74baa7e9cc44dd2afa03
Holt Skinner <holtskinner@google.com>
2023-01-24 10:17:07 -0600

    docs(samples): Added Testing based on review comments - Changed Sample name to Quickstart

---
+++ documentai/api/DocumentAI.Samples.Tests/DocumentAIFixture.cs
@@ -0,0 +1,65 @@
+/*
+ * Copyright 2023 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+
+using Google.Api.Gax.ResourceNames;
+using Google.Cloud.DocumentAI.V1;
+using Google.Protobuf.WellKnownTypes;
+using System;
+using System.Threading;
+using Xunit;
+
+[CollectionDefinition(nameof(DocumentAIFixture))]
+public class DocumentAIFixture : IDisposable, ICollectionFixture<DocumentAIFixture>
+{
+    public string ProjectId { get; }
+    public ProjectName ProjectName { get; }
+
+    public string LocationId { get; }
+    public LocationName LocationName { get; }
+
+    public string ProcessorId { get; }
+    public ProcessorName ProcessorName { get; }
+
+    public string LocalPath { get; }
+
+    public string MimeType { get; }
+
+    public DocumentAIFixture()
+    {
+        ProjectId = Environment.GetEnvironmentVariable("GOOGLE_PROJECT_ID");
+        if (string.IsNullOrEmpty(ProjectId))
+        {
+            throw new Exception("missing GOOGLE_PROJECT_ID");
+        }
+        ProjectName = new ProjectName(ProjectId);
+
+        LocationId = "us";
+        LocationName = new LocationName(ProjectId, LocationId);
+
+        ProcessorId = "aaaaaaaaaa";
+        ProcessorName = ProcessorName.FromProjectLocationProcessor(ProjectId, LocationId, ProcessorId);
+
+        LocalPath = "Resources/Invoice.pdf";
+
+        MimeType = "application/pdf";
+    }
+
+    public void Dispose()
+    {
+    }
+
+}
---
+++ documentai/api/DocumentAI.Samples/Quickstart.cs
@@ -0,0 +1,62 @@
+/*
+ * Copyright 2023 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+// [START documentai_process_document]
+// [START documentai_quickstart]
+
+using Google.Cloud.DocumentAI.V1;
+using Google.Protobuf;
+using System;
+using System.IO;
+
+public class QuickstartSample
+{
+    public void Quickstart(
+        string projectId = "your-project-id",
+        string locationId = "your-processor-location",
+        string processorId = "your-processor-id",
+        string localPath = "my-local-path/my-file-name",
+        string mimeType = "application/pdf"
+    )
+    {
+        // Create client
+        DocumentProcessorServiceClient client = DocumentProcessorServiceClient.Create();
+
+        // Read in local file
+        using var fileStream = File.OpenRead(localPath);
+        var rawDocument = new RawDocument
+        {
+            Content = ByteString.FromStream(fileStream),
+            MimeType = mimeType
+        };
+
+        // Initialize request argument(s)
+        var request = new ProcessRequest
+        {
+            Name = ProcessorName.FromProjectLocationProcessor(projectId, locationId, processorId).ToString(),
+            RawDocument = rawDocument
+        };
+
+        // Make the request
+        var response = client.ProcessDocument(request);
+
+        var document = response.Document;
+        Console.WriteLine($"{document.Text}");
+    }
+}
+
+// [END documentai_quickstart]
+// [END documentai_process_document]
