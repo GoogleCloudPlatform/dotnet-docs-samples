f3e6ecad7338798085f6487fbb4d3e47e07d769b
Hemanshv <hemanshverma@google.com>
2023-03-15 11:21:22 +0530

    feat: Add sample to listen error pubsub

---
+++ pubsub/api/Pubsub.Samples.Tests/PullMessagesAsyncWithErrorListenTest.cs
@@ -0,0 +1,48 @@
+﻿// Copyright 2023 Google Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Pubsub.Samples.Tests;
+
+[Collection(nameof(PubsubFixture))]
+public class PullMessagesAsyncWithErrorListenTest
+{
+    private readonly PubsubFixture _pubsubFixture;
+    private readonly PublishMessagesAsyncSample _publishMessagesAsyncSample;
+    private readonly PullMessagesAsyncWithErrorListen _pullMessagesWithErrorListenSample;
+
+    public PullMessagesAsyncWithErrorListenTest(PubsubFixture pubsubFixture)
+    {
+        _pubsubFixture = pubsubFixture;
+        _publishMessagesAsyncSample = new PublishMessagesAsyncSample();
+        _pullMessagesWithErrorListenSample = new PullMessagesAsyncWithErrorListen();
+    }
+
+    [Fact]
+    public async Task PullMessagesWithErrorAsyncTest()
+    {
+        string randomName = _pubsubFixture.RandomName();
+        string topicId = $"testTopicForMessageAck{randomName}";
+        string subscriptionId = $"testSubscriptionForMessageAck{randomName}";
+        var message = _pubsubFixture.RandomName();
+
+        _pubsubFixture.CreateTopic(topicId);
+        // Intentionally not creating the subscription, want pull to throw some unrecoverable exception.
+        await _publishMessagesAsyncSample.PublishMessagesAsync(_pubsubFixture.ProjectId, topicId, new string[] { message });
+        var result = await _pullMessagesWithErrorListenSample.PullMessagesAsyncListenError(_pubsubFixture.ProjectId, subscriptionId, true);
+        Assert.Equal(0, result);
+    }
+}
---
+++ pubsub/api/Pubsub.Samples/PullMessagesAsyncWithErrorListen.cs
@@ -0,0 +1,54 @@
+﻿// Copyright 2020 Google Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// [START pubsub_subscriber_error_listener]
+
+using Google.Cloud.PubSub.V1;
+using System;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+public class PullMessagesAsyncWithErrorListen
+{
+    public async Task<int> PullMessagesAsyncListenError(string projectId, string subscriptionId, bool acknowledge)
+    {
+        SubscriptionName subscriptionName = SubscriptionName.FromProjectSubscription(projectId, subscriptionId);
+        SubscriberClient subscriber = await SubscriberClient.CreateAsync(subscriptionName);
+        // SubscriberClient runs your message handle function on multiple
+        // threads to maximize throughput.
+        int messageCount = 0;
+        Task startTask = subscriber.StartAsync((PubsubMessage message, CancellationToken cancel) =>
+        {
+            string text = System.Text.Encoding.UTF8.GetString(message.Data.ToArray());
+            Console.WriteLine($"Message {message.MessageId}: {text}");
+            Interlocked.Increment(ref messageCount);
+            return Task.FromResult(acknowledge ? SubscriberClient.Reply.Ack : SubscriberClient.Reply.Nack);
+        }).ContinueWith(task =>
+        {
+            Console.WriteLine("Exception while listening msgs: ", task.Exception.InnerException.Message);
+        }, TaskContinuationOptions.OnlyOnFaulted);
+
+        // Run for 5 seconds.
+        await Task.Delay(5000);
+        await subscriber.StopAsync(CancellationToken.None).ContinueWith(task =>
+        {
+            Console.WriteLine("Exception while stoping subscriber: ", task.Exception.InnerException.Message);
+        }, TaskContinuationOptions.OnlyOnFaulted); ;
+        // Lets make sure that the start task finished successfully after the call to stop.
+        await startTask;
+        return messageCount;
+    }
+}
+// [END pubsub_subscriber_error_listener]
