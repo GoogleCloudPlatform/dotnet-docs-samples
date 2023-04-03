ccf428c80fc6a74c8755dbb10d9aeb1072ba2201
jainamtrivedi_crest <jainam.trivedi@crestdatasys.com>
2023-04-03 18:12:47 +0530

    Implemented sample of inspecting data with custom regex

---
+++ dlp/api/Snippets.Tests/InspectDataWithCustomRegexTests.cs
@@ -0,0 +1,38 @@
+﻿// Copyright 2023 Google Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License
+
+using Google.Cloud.Dlp.V2;
+using Xunit;
+
+namespace GoogleCloudSamples
+{
+    public class InspectDataWithCustomRegexTests : IClassFixture<DlpTestFixture>
+    {
+        private DlpTestFixture _fixture;
+        public InspectDataWithCustomRegexTests(DlpTestFixture fixture) => _fixture = fixture;
+
+        [Fact]
+        public void TestInspectDataWithCustomRegex()
+        {
+            string mrn = "444-5-22222";
+            string textToInspect = $"Patients MRN {mrn}";
+            string customRegexPattern = "[1-9]{3}-[1-9]{1}-[1-9]{5}";
+            var infoType = new InfoType { Name = "C_MRN" };
+            var result = InspectDataWithCustomRegex.InspectDataCustomRegex(_fixture.ProjectId, textToInspect, customRegexPattern, infoType);
+            var findings = result.Result.Findings;
+            var finding = Assert.Single(findings);
+            Assert.Equal(mrn, finding.Quote);
+        }
+    }
+}
---
+++ dlp/api/Snippets/InspectDataWithCustomRegex.cs
@@ -0,0 +1,76 @@
+﻿// Copyright 2023 Google Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License"); you may not
+// use this file except in compliance with the License. You may obtain a copy of
+// the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+// WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+// License for the specific language governing permissions and limitations under
+// the License.
+
+// [START dlp_inspect_custom_regex]
+
+using System;
+using Google.Api.Gax.ResourceNames;
+using Google.Cloud.Dlp.V2;
+
+public class InspectDataWithCustomRegex
+{
+    public static InspectContentResponse InspectDataCustomRegex(
+        string projectId,
+        string text,
+        string customRegex,
+        InfoType infoType = null)
+    {
+        // Instantiate a client.
+        var dlp = DlpServiceClient.Create();
+
+        // Construct content item by setting the text.
+        var contentItem = new ContentItem { Value = text };
+
+        // Construct the custom regex detector.
+        var customInfoType = new CustomInfoType
+        {
+            InfoType = infoType ?? new InfoType { Name = "C_MRN" },
+            Regex = new CustomInfoType.Types.Regex { Pattern = customRegex }
+        };
+
+        // Construct Inspect Config.
+        var inspectConfig = new InspectConfig
+        {
+            CustomInfoTypes = { customInfoType },
+            IncludeQuote = true,
+            MinLikelihood = Likelihood.Possible
+        };
+
+        // Construct the request.
+        var request = new InspectContentRequest
+        {
+            ParentAsLocationName = new LocationName(projectId, "global"),
+            Item = contentItem,
+            InspectConfig = inspectConfig,
+        };
+
+        // Call the API.
+        var response = dlp.InspectContent(request);
+
+        // Inspect the results.
+        var resultFindings = response.Result.Findings;
+
+        Console.WriteLine($"Findings: {resultFindings.Count}");
+        foreach (var f in resultFindings)
+        {
+            Console.WriteLine("Quote: " + f.Quote);
+            Console.WriteLine("Info type: " + f.InfoType.Name);
+            Console.WriteLine("Likelihood: " + f.Likelihood);
+        }
+
+        return response;
+    }
+}
+
+// [END dlp_inspect_custom_regex]
