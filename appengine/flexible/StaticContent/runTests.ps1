022763960d5e5c1d41583c6855a97cad8efe689c
jahjahbin <jahoak@google.com>
2023-05-09 10:44:35 -0700

    Start revamping static sample

--- appengine/flexible/StaticContent/runTests.ps1
+++ appengine/flexible/StaticContent/runTests.ps1
@@ -12,8 +12,5 @@
 # License for the specific language governing permissions and limitations under
 # the License.
 
-Import-Module -DisableNameChecking ..\..\..\BuildTools.psm1
-
 dotnet restore --force
-dotnet build --no-restore
-Run-KestrelTest 5557 -CasperJs11
+dotnet test --no-restore --test-adapter-path:. --logger:junit 2>&1 | %{ "$_" }
