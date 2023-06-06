00e7a46d0cd1196769a9622cccd1c67ff50198ad
jahjahbin <jahoak@google.com>
2023-06-05 17:32:01 -0700

    No more creating stuff

--- appengine/flexible/Pubsub/Pubsub.Sample/Program.cs
+++ appengine/flexible/Pubsub/Pubsub.Sample/Program.cs
@@ -37,7 +37,7 @@ public class Program
             ServiceAccountEmail = builder.Configuration["TEST_SERVICE_ACCOUNT_EMAIL"]
         };
         try {
-            var topic = CreateTopicAndSubscription(options);
+            var topic = new TopicName(options.ProjectId, options.TopicId);
             builder.Services.AddSingleton(Options.Create(options));
             builder.Services.AddSingleton(PublisherClient.Create(topic));
             builder.Services.AddMvc();
@@ -52,76 +52,6 @@ public class Program
             App = builder.Build();
             App.MapGet("/", () => "Pubsub setup failed, please ensure the code sample's environment variables are set.");
         }
-    }
-
-    static TopicName CreateTopicAndSubscription(PubsubOptions options)
-    {
-        var topicName = new TopicName(options.ProjectId, options.TopicId);
-        var publisher = PublisherServiceApiClient.Create();
-        try
-        {
-            publisher.CreateTopic(topicName);
-        }
-        catch (Grpc.Core.RpcException e)
-        when (e.Status.StatusCode == Grpc.Core.StatusCode.AlreadyExists ||
-            e.Status.StatusCode == Grpc.Core.StatusCode.PermissionDenied)
-        {
-            Console.WriteLine("Could not create topic {0}", topicName);
-        }
-        // Create push subscription for /Push endpoint.
-        var subscriptionName = new SubscriptionName(
-                options.ProjectId, options.SubscriptionId);
-        var pushConfig = new PushConfig()
-        {
-            PushEndpoint = $"https://{options.ProjectId}.appspot.com/Push"
-        };
-        CreateOrModifySubscription(subscriptionName, topicName, pushConfig);
-
-        // Create push subscription for /AuthPush endpoint.
-        var authSubscriptionName = new SubscriptionName(
-            options.ProjectId, options.AuthSubscriptionId);
-        // To enable authentication in subscription.
-        var oidcToken = new PushConfig.Types.OidcToken();
-        oidcToken.ServiceAccountEmail = options.ServiceAccountEmail;
-        var authPushConfig = new PushConfig
-        {
-            PushEndpoint = $"https://{options.ProjectId}.appspot.com/AuthPush",
-            OidcToken = oidcToken
-        };
-        CreateOrModifySubscription(authSubscriptionName, topicName, authPushConfig);
-        return topicName;
-    }
-
-    static void CreateOrModifySubscription(SubscriptionName subscriptionName,TopicName topicName,PushConfig pushConfig)
-    {
-        var subscriber = SubscriberServiceApiClient.Create();
-        try
-        {
-            subscriber.CreateSubscription(subscriptionName, topicName,
-                pushConfig, 20);
-            return;
-        }
-        catch (Grpc.Core.RpcException e)
-        when (e.Status.StatusCode == Grpc.Core.StatusCode.AlreadyExists ||
-            e.Status.StatusCode == Grpc.Core.StatusCode.PermissionDenied)
-        {
-            Console.WriteLine("Could not create subscription {0}", subscriptionName);
-        }
-        catch (Grpc.Core.RpcException e)
-        {
-            Console.WriteLine("Could not create subscription {0}", subscriptionName);
-            return;
-        }
-        try
-        {
-            subscriber.ModifyPushConfig(subscriptionName, pushConfig);
-        }
-        catch (Grpc.Core.RpcException e)
-        when (e.Status.StatusCode == Grpc.Core.StatusCode.PermissionDenied)
-        {
-            Console.WriteLine("Could not modify subscription {0}", subscriptionName);
-        }
-        return;
     }
 
     public static void Main(string[] args)
