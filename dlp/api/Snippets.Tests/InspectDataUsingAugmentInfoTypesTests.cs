59162a15b657b329fdb264b9e9aa4b726b0606ed
jainamtrivedi-crest <jainam.trivedi@crestdatasys.com>
2023-08-09 17:24:49 +0530

    Implemented sample for inspect data using augment infotype and test cases

---
+++ dlp/api/Snippets.Tests/InspectDataUsingAugmentInfoTypesTests.cs
@@ -0,0 +1,33 @@
+﻿// Copyright 2023 Google Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License
+
+using Xunit;
+
+namespace GoogleCloudSamples
+{
+    public class InspectDataUsingAugmentInfoTypesTests : IClassFixture<DlpTestFixture>
+    {
+        private DlpTestFixture _fixture;
+        public InspectDataUsingAugmentInfoTypesTests(DlpTestFixture fixture) => _fixture = fixture;
+
+        [Fact]
+        public void TestAugmentInfoType()
+        {
+            var text = "The patient's name is Quasimodo.";
+            var result = InspectDataUsingAugmentInfoTypes.InspectData(_fixture.ProjectId, text);
+            Assert.Equal(2, result.Result.Findings.Count);
+            Assert.Contains(result.Result.Findings, f => f.InfoType.Name == "PERSON_NAME");
+        }
+    }
+}
