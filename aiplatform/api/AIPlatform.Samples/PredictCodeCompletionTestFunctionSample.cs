5fca3dbd3b26741e3443506d4cc2cd85d94e23fd
Mete Atamel <atamel@google.com>
2023-11-21 16:54:17 +0000

    docs(aiplatform): Add PredictCodeCompletionTestFunctionSample #2393 (#2404)

---
+++ aiplatform/api/AIPlatform.Samples/PredictCodeCompletionTestFunctionSample.cs
@@ -0,0 +1,83 @@
+/*
+ * Copyright 2023 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+// [START aiplatform_sdk_code_completion_test_function]
+
+using Google.Cloud.AIPlatform.V1;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Value = Google.Protobuf.WellKnownTypes.Value;
+
+public class PredictCodeCompletionTestFunctionSample
+{
+    public string PredictTestFunction(
+        string projectId = "your-project-id",
+        string locationId = "us-central1",
+        string publisher = "google",
+        string model = "code-gecko@001")
+    {
+        // Initialize client that will be used to send requests.
+        // This client only needs to be created once,
+        // and can be reused for multiple requests.
+        var client = new PredictionServiceClientBuilder
+        {
+            Endpoint = $"{locationId}-aiplatform.googleapis.com"
+        }.Build();
+
+        // Configure the parent resource.
+        var endpoint = EndpointName.FromProjectLocationPublisherModel(projectId, locationId, publisher, model);
+
+        var prefix = @"
+public static string ReverseString(string s)
+{
+    char[] chars = s.ToCharArray();
+    Array.Reverse(chars);
+    return new string(chars);
+}
+public static void TestEmptyInputString()";
+
+        var instances = new List<Value>
+        {
+            Value.ForStruct(new()
+            {
+                Fields =
+                {
+                    ["prefix"] = Value.ForString(prefix),
+                }
+            })
+        };
+
+        var parameters = Value.ForStruct(new()
+        {
+            Fields =
+            {
+                { "temperature", new Value { NumberValue = 0.2 } },
+                { "maxOutputTokens", new Value { NumberValue = 64 } }
+            }
+        });
+
+        // Make the request.
+        var response = client.Predict(endpoint, instances, parameters);
+
+        // Parse and return the content.
+        var content = response.Predictions.First().StructValue.Fields["content"].StringValue;
+        Console.WriteLine($"Content: {content}");
+        return content;
+    }
+}
+
+// [END aiplatform_sdk_code_completion_test_function]
