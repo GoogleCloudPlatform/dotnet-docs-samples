996a450f5e7813115514fff53d7c97c5b2536561
Mete Atamel <atamel@google.com>
2023-11-21 16:52:18 +0000

    docs(aiplatform): Add PredictCodeGenerationFunctionSample #2394 (#2405)

---
+++ aiplatform/api/AIPlatform.Samples.Tests/PredictCodeGenerationFunctionTest.cs
@@ -0,0 +1,37 @@
+/*
+ * Copyright 2023 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+using Xunit;
+
+[Collection(nameof(AIPlatformFixture))]
+public class PredictCodeGenerationFunctionTest
+{
+    private readonly AIPlatformFixture _fixture;
+    private readonly PredictCodeGenerationFunctionSample _sample;
+
+    public PredictCodeGenerationFunctionTest(AIPlatformFixture fixture)
+    {
+        _fixture = fixture;
+        _sample = new PredictCodeGenerationFunctionSample();
+    }
+
+    [Fact]
+    public void TestPredictFunction()
+    {
+        var response = _sample.PredictFunction(_fixture.ProjectId);
+        Assert.NotNull(response);
+    }
+}
