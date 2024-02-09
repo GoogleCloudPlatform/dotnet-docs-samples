4c8ae0c8b144c7bf4ec1252c8a4f08f585571543
Jon Skeet <jonskeet@google.com>
2024-02-09 12:03:23 +0000

    docs(pubsub): Add pubsub_delete_schema_revision sample

---
+++ pubsub/api/Pubsub.Samples.Tests/DeleteSchemaRevisionTest.cs
@@ -0,0 +1,43 @@
+// Copyright 2024 Google Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+using Xunit;
+
+[Collection(nameof(PubsubFixture))]
+public class DeleteSchemaRevisionTest
+{
+    private readonly PubsubFixture _pubsubFixture;
+    private readonly CommitAvroSchemaSample _commitAvroSchemaSample;
+    private readonly DeleteSchemaRevisionSample _deleteSchemaRevisionSample;
+
+    public DeleteSchemaRevisionTest(PubsubFixture pubsubFixture)
+    {
+        _pubsubFixture = pubsubFixture;
+        _commitAvroSchemaSample = new CommitAvroSchemaSample();
+        _deleteSchemaRevisionSample = new DeleteSchemaRevisionSample();
+    }
+
+    [Fact]
+    public void DeleteSchemaRevision()
+    {
+        string schemaId = _pubsubFixture.RandomSchemaId();
+        var initialSchema = _pubsubFixture.CreateAvroSchema(schemaId);
+        var updatedSchema = _commitAvroSchemaSample.CommitAvroSchema(_pubsubFixture.ProjectId, schemaId, _pubsubFixture.NewAvroSchemaFile);
+        _deleteSchemaRevisionSample.DeleteSchemaRevision(_pubsubFixture.ProjectId, schemaId, initialSchema.RevisionId);
+
+        var allRevisions = _pubsubFixture.ListSchemaRevisions(schemaId);
+        var remainingRevision = Assert.Single(allRevisions);
+        Assert.Equal(updatedSchema.RevisionId, remainingRevision.RevisionId);
+    }
+}
