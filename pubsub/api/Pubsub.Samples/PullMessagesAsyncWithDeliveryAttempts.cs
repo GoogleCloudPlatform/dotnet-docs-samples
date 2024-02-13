bc5c293528e36b7b0fcc78f439bf0a864eedfa5c
Jon Skeet <jonskeet@google.com>
2024-02-13 12:54:54 +0000

    chore: Remove unnecessary using directives

--- pubsub/api/Pubsub.Samples/PullMessagesAsyncWithDeliveryAttempts.cs
+++ pubsub/api/Pubsub.Samples/PullMessagesAsyncWithDeliveryAttempts.cs
@@ -1,4 +1,4 @@
-﻿// Copyright 2020 Google Inc.
+// Copyright 2020 Google Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -16,7 +16,6 @@
 
 using Google.Cloud.PubSub.V1;
 using System.Linq;
-using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 
--- testutil/TestUtil.cs
+++ testutil/TestUtil.cs
@@ -17,7 +17,6 @@
 using System;
 using System.Collections.Generic;
 using System.IO;
-using System.Linq;
 using System.Security.Cryptography;
 using System.Threading;
 using System.Threading.Tasks;
