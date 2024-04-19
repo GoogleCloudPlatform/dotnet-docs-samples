365b2eed3524ee6e76f3363dbab1070d1b428e18
Amanda Tarafa Mas <atarafamas@google.com>
2024-04-19 00:33:05 -0700

    fix[Spanner]: Update commit delay sample.
    To use "MaxCommitDelay".

--- spanner/api/Spanner.Samples/CommitDelayAsync.cs
+++ spanner/api/Spanner.Samples/CommitDelayAsync.cs
@@ -29,7 +29,7 @@ public class CommitDelayAsyncSample
 
         return await connection.RunWithRetriableTransactionAsync(async transaction =>
         {
-            transaction.CommitDelay = TimeSpan.FromMilliseconds(100);
+            transaction.MaxCommitDelay = TimeSpan.FromMilliseconds(100);
 
             using var insertSingerCmd = connection.CreateInsertCommand("Singers",
                 new SpannerParameterCollection
