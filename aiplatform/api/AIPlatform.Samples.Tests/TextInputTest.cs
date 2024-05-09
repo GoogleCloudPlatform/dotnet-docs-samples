97048a2585256c72da9fd6c5f043a83b0e48a1f3
Nick Cook <nickcook@google.com>
2024-05-08 22:52:37 -0700

    feat: add gemini code sample and test for text-only input

---
+++ aiplatform/api/AIPlatform.Samples.Tests/TextInputTest.cs
@@ -0,0 +1,39 @@
+/*
+ * Copyright 2024 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+using System;
+using System.Threading.Tasks;
+using Xunit;
+
+[Collection(nameof(AIPlatformFixture))]
+public class TextInputTest
+{
+    private readonly AIPlatformFixture _fixture;
+    private readonly TextInputSample _sample;
+
+    public TextInputTest(AIPlatformFixture fixture)
+    {
+        _fixture = fixture;
+        _sample = new TextInputSample();
+    }
+
+    [Fact]
+    public async Task TestDescribeText()
+    {
+        var response = await _sample.TextInput(_fixture.ProjectId);
+        Assert.Contains("positive", response, StringComparison.OrdinalIgnoreCase);
+    }
+}
