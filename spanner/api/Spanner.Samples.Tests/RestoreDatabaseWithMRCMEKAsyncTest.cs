2aae2df0e61849062d7056a9bb1668c5b09ff5c0
panerorenn9541 <36008213+panerorenn9541@users.noreply.github.com>
2024-08-28 18:25:50 -0700

    Create RestoreDatabaseWithMRCMEKAsyncTest.cs

---
+++ spanner/api/Spanner.Samples.Tests/RestoreDatabaseWithMRCMEKAsyncTest.cs
@@ -0,0 +1,45 @@
+// Copyright 2021 Google Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+using Google.Cloud.Spanner.Admin.Database.V1;
+using System;
+using System.Threading.Tasks;
+using Xunit;
+
+/// <summary>
+/// Tests restoring a databases using customer managed encryption.
+/// </summary>
+[Collection(nameof(SpannerFixture))]
+public class RestoreDatabaseWithMRCMEKAsyncTest
+{
+    private readonly SpannerFixture _fixture;
+
+    public RestoreDatabaseWithMRCMEKAsyncTest(SpannerFixture fixture)
+    {
+        _fixture = fixture;
+    }
+
+    [SkippableFact]
+    public async Task TestRestoreDatabaseWithMRCMEKAsync()
+    {
+        Skip.If(!_fixture.RunCmekBackupSampleTests, SpannerFixture.SkipCmekBackupSamplesMessage);
+        var sample = new RestoreDatabaseWithMRCMEKAsyncSample();
+        var database = await sample.RestoreDatabaseWithMRCMEKAsync(_fixture.ProjectId, _fixture.InstanceId, _fixture.EncryptedRestoreDatabaseId, _fixture.FixedEncryptedBackupId, _fixture.KmsKeyName);
+        Assert.Equal(_fixture.KmsKeyNames.Length, database.EncryptionConfig.KmsKeyNames.Length);
+        foreach (string KmsKey in database.EncryptionConfig.KmsKeyNames)
+        {
+          Assert.True(_fixture.KmsKeyNames.contains(CryptoKeyName.Parse(KmsKey)));
+        }
+    }
+}
