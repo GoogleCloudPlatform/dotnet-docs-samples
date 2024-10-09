ba3c128633f7fb9f4686af3893820a60d3050ffe
mahendra-google <mahendraw@google.com>
2024-10-09 01:50:09 -0700

    feat(Storage Transfer) : Addition of Sample and Test Case for storagetransfer_transfer_to_nearline.

---
+++ storagetransfer/api/StorageTransfer.Samples.Tests/TransferToNearlineTest.cs
@@ -0,0 +1,67 @@
+// Copyright 2024 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License").
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at 
+//
+// https://www.apache.org/licenses/LICENSE-2.0 
+//
+// Unless required by applicable law or agreed to in writing, software 
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and 
+// limitations under the License.
+
+
+using Google.Cloud.Storage.V1;
+using Google.Cloud.StorageTransfer.V1;
+using System;
+using Xunit;
+
+namespace StorageTransfer.Samples.Tests;
+
+[Collection(nameof(StorageFixture))]
+public class TransferToNearlineTest : IDisposable
+{
+    private readonly StorageFixture _fixture;
+    private string _transferJobName;
+    public TransferToNearlineTest(StorageFixture fixture)
+    {
+        _fixture = fixture;
+    }
+
+    [Fact]
+    public void TestTransferToNearline()
+    {
+        TransferToNearlineSample transferToNearlineSample = new TransferToNearlineSample();
+        var storage = StorageClient.Create();
+        var bucket = storage.GetBucket(_fixture.BucketNameSink);
+        string storageClass = StorageClasses.Nearline;
+        bucket.StorageClass = storageClass;
+        bucket = storage.UpdateBucket(bucket);
+        var transferJob = transferToNearlineSample.TransferToNearline(_fixture.ProjectId, _fixture.BucketNameSource, _fixture.BucketNameSink);
+        Assert.Contains("transferJobs/", transferJob.Name);
+        _transferJobName = transferJob.Name;
+    }
+
+    public void Dispose()
+    {
+        try
+        {
+            _fixture.Sts.UpdateTransferJob(new UpdateTransferJobRequest()
+            {
+                ProjectId = _fixture.ProjectId,
+                JobName = _transferJobName,
+                TransferJob = new TransferJob()
+                {
+                    Name = _transferJobName,
+                    Status = TransferJob.Types.Status.Deleted
+                }
+            });
+        }
+        catch (Exception)
+        {
+            // Do nothing, we delete on a best effort basis.
+        }
+    }
+}
---
+++ storagetransfer/api/StorageTransfer.Samples/TransferToNearlineSample.cs
@@ -0,0 +1,68 @@
+// Copyright 2024 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License").
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at 
+//
+// https://www.apache.org/licenses/LICENSE-2.0 
+//
+// Unless required by applicable law or agreed to in writing, software 
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and 
+// limitations under the License.
+// [START storagetransfer_transfer_to_nearline]
+
+using Google.Cloud.StorageTransfer.V1;
+using Google.Protobuf.WellKnownTypes;
+using System;
+
+
+namespace StorageTransfer.Samples
+{
+
+    public class TransferToNearlineSample
+    {
+        /*Creates a one-off transfer job that transfers objects from a standard GCS bucket that are more
+         than 30 days old to a Nearline GCS bucket.*/
+        public TransferJob TransferToNearline(
+            // Your Google Cloud Project ID
+            string projectId = "my-project-id",
+            // The GCS bucket to transfer objects from
+            string sourceBucket = "my-source-bucket",
+            // The GCS Nearline bucket to transfer old objects to
+            string sinkBucket = "my-sink-bucket")
+        {
+            // A description of this job
+            string jobDescription = $"Transfers old objects from standard bucket ({sourceBucket}) that haven't been modified in the last 30 days to a Nearline bucket ({sinkBucket})";
+
+            TransferJob transferJob = new TransferJob
+            {
+                ProjectId = projectId,
+                Description = jobDescription,
+                TransferSpec = new TransferSpec
+                {
+                    GcsDataSink = new GcsData { BucketName = sinkBucket },
+                    GcsDataSource = new GcsData { BucketName = sourceBucket },
+                    ObjectConditions = new ObjectConditions { MinTimeElapsedSinceLastModification = Duration.FromTimeSpan(TimeSpan.FromSeconds(2592000)) },
+                    TransferOptions = new TransferOptions { DeleteObjectsFromSourceAfterTransfer = true },
+                },
+                Status = TransferJob.Types.Status.Enabled,
+                Schedule = new Schedule { ScheduleStartDate = Google.Type.Date.FromDateTime(System.DateTime.UtcNow.Date.AddMonths(1)) }
+            };
+            // Create a Transfer Service client
+            StorageTransferServiceClient client = StorageTransferServiceClient.Create();
+            // Create a Transfer job
+            TransferJob response = client.CreateTransferJob(new CreateTransferJobRequest { TransferJob = transferJob });
+            client.RunTransferJob(new RunTransferJobRequest
+            {
+                JobName = response.Name,
+                ProjectId = projectId
+            });
+
+            Console.WriteLine($"Created transfer job from standard bucket {sourceBucket} to Nearline bucket {sinkBucket} with name {response.Name}");
+            return response;
+        }
+    }
+}
+// [END storagetransfer_transfer_to_nearline]
