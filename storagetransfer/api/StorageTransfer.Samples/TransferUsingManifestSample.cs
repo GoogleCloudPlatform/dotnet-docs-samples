f5d8252eb0b6984f5681cc296a3817c8a91824f7
Mahendra <mahendraw@google.com>
2024-12-19 14:49:12 +0530

    Update storagetransfer/api/StorageTransfer.Samples/TransferUsingManifestSample.cs
    Co-authored-by: Amanda Tarafa Mas <14878252+amanda-tarafa@users.noreply.github.com>

--- storagetransfer/api/StorageTransfer.Samples/TransferUsingManifestSample.cs
+++ storagetransfer/api/StorageTransfer.Samples/TransferUsingManifestSample.cs
@@ -11,6 +11,7 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and 
 // limitations under the License.
+
 // [START storagetransfer_manifest_request]
 using Google.Cloud.StorageTransfer.V1;
 using System;
