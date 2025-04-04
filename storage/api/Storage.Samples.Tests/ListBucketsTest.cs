09b32b5d4e2eb4cbcf4aa53bfa8ba7be55695872
mahendra-google <mahendraw@google.com>
2025-04-04 06:28:48 -0700

    listbucket test undo changes

--- storage/api/Storage.Samples.Tests/ListBucketsTest.cs
+++ storage/api/Storage.Samples.Tests/ListBucketsTest.cs
@@ -12,11 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-using Google.Apis.Storage.v1.Data;
-using Google.Cloud.Storage.V1;
 using System;
-using System.Threading;
-using System.Threading.Tasks;
 using Xunit;
 
 [Collection(nameof(StorageFixture))]
