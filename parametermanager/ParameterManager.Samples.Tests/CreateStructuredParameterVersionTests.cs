3a7c934245bfa95b94391e1eafa3f98138603ee5
durgesh-ninave-crest <durgesh.ninave@crestdata.ai>
2025-04-09 15:47:13 +0530

    feat(parametermanager): Added samples for create global parameter and parameter version

---
+++ parametermanager/ParameterManager.Samples.Tests/CreateStructuredParameterVersionTests.cs
@@ -0,0 +1,42 @@
+/*
+ * Copyright 2025 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+using Google.Cloud.ParameterManager.V1;
+
+[Collection(nameof(ParameterManagerFixture))]
+public class CreateStructureParameterVersionTests
+{
+    private readonly ParameterManagerFixture _fixture;
+    private readonly CreateStructuredParameterVersionSample _sample;
+
+    public CreateStructureParameterVersionTests(ParameterManagerFixture fixture)
+    {
+        _fixture = fixture;
+        _sample = new CreateStructuredParameterVersionSample();
+    }
+
+    [Fact]
+    public void CreateStructureParameterVersion()
+    {
+        ParameterVersionName parameterVersionName = _fixture.ParameterVersionNameWithFormat;
+        string payload = ParameterManagerFixture.JsonPayload;
+        ParameterVersion result = _sample.CreateStructuredParameterVersion(
+          projectId: parameterVersionName.ProjectId, parameterId: parameterVersionName.ParameterId, versionId: parameterVersionName.ParameterVersionId, payload: payload);
+
+        Assert.NotNull(result);
+        Assert.Equal(result.ParameterVersionName.ParameterVersionId, parameterVersionName.ParameterVersionId);
+    }
+}
