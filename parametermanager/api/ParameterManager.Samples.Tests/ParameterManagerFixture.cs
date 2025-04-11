bb345bcfd7e97c58f168a2b369da74622434001f
durgesh-ninave-crest <durgesh.ninave@crestdata.ai>
2025-04-11 16:14:10 +0530

    feat(parametermanager): Added samples for delete, disable and enable global parameter

---
+++ parametermanager/api/ParameterManager.Samples.Tests/ParameterManagerFixture.cs
@@ -0,0 +1,111 @@
+// Copyright(c) 2025 Google Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License"); you may not
+// use this file except in compliance with the License. You may obtain a copy of
+// the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+// WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+// License for the specific language governing permissions and limitations under
+// the License.
+
+using Google.Api.Gax.ResourceNames;
+using Google.Cloud.Iam.V1;
+using Google.Cloud.ParameterManager.V1;
+using Google.Cloud.SecretManager.V1;
+using Google.Protobuf;
+using System.Text;
+
+[CollectionDefinition(nameof(ParameterManagerFixture))]
+public class ParameterManagerFixture : IDisposable, ICollectionFixture<ParameterManagerFixture>
+{
+    public string ProjectId { get; }
+    public const string LocationId = "global";
+
+    internal List<ParameterName> ParametersToDelete = new List<ParameterName>();
+    internal List<ParameterVersionName> ParameterVersionsToDelete = new List<ParameterVersionName>();
+
+    public ParameterManagerFixture()
+    {
+        ProjectId = Environment.GetEnvironmentVariable("GOOGLE_PROJECT_ID");
+        if (String.IsNullOrEmpty(ProjectId))
+        {
+            throw new Exception("missing GOOGLE_PROJECT_ID");
+        }
+    }
+
+    public void Dispose()
+    {
+        foreach (var parameterVersion in ParameterVersionsToDelete)
+        {
+            DeleteParameterVersion(parameterVersion);
+        }
+        foreach (var parameter in ParametersToDelete)
+        {
+            DeleteParameter(parameter);
+        }
+    }
+
+    public String RandomId()
+    {
+        return $"csharp-{System.Guid.NewGuid()}";
+    }
+
+    public Parameter CreateParameter(string parameterId, ParameterFormat format)
+    {
+        ParameterManagerClient client = ParameterManagerClient.Create();
+        LocationName projectName = new LocationName(ProjectId, LocationId);
+
+        Parameter parameter = new Parameter
+        {
+            Format = format
+        };
+
+        return client.CreateParameter(projectName, parameter, parameterId);
+    }
+
+    public ParameterVersion CreateParameterVersion(string parameterId, string versionId, string payload)
+    {
+        ParameterManagerClient client = ParameterManagerClient.Create();
+
+        ParameterName parameterName = new ParameterName(ProjectId, LocationId, parameterId);
+        ParameterVersion parameterVersion = new ParameterVersion
+        {
+            Payload = new ParameterVersionPayload
+            {
+                Data = ByteString.CopyFrom(payload, Encoding.UTF8)
+            }
+        };
+
+        return client.CreateParameterVersion(parameterName, parameterVersion, versionId);
+    }
+
+    private void DeleteParameter(ParameterName name)
+    {
+        ParameterManagerClient client = ParameterManagerClient.Create();
+        try
+        {
+            client.DeleteParameter(name);
+        }
+        catch (Grpc.Core.RpcException e) when (e.StatusCode == Grpc.Core.StatusCode.NotFound)
+        {
+            // Ignore error - Parameter was already deleted
+        }
+    }
+
+    private void DeleteParameterVersion(ParameterVersionName name)
+    {
+        ParameterManagerClient client = ParameterManagerClient.Create();
+        try
+        {
+            client.DeleteParameterVersion(name);
+        }
+        catch (Grpc.Core.RpcException e) when (e.StatusCode == Grpc.Core.StatusCode.NotFound)
+        {
+            // Ignore error - Parameter version was already deleted
+        }
+    }
+}
