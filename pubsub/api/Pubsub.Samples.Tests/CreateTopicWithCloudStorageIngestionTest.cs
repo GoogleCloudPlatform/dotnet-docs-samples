30526a8aa7a5a23c33778889229c952e1b88147e
Charlotte Y <yeji.yun1225@gmail.com>
2025-05-09 15:11:30 -0700

    feat(PubSub): Add CreateTopicWithCloudStorageIngestion Sample

---
+++ pubsub/api/Pubsub.Samples.Tests/CreateTopicWithCloudStorageIngestionTest.cs
@@ -0,0 +1,54 @@
+// Copyright 2025 Google LLC.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+using Google.Cloud.PubSub.V1;
+using Google.Protobuf.WellKnownTypes;
+using System;
+using Xunit;
+
+[Collection(nameof(PubsubFixture))]
+public class CreateTopicWithCloudStorageIngestionTest
+{
+    private readonly PubsubFixture _pubsubFixture;
+    private readonly CreateTopicWithCloudStorageIngestionSample _createTopicWithCloudStorageIngestionSample;
+
+    public CreateTopicWithCloudStorageIngestionTest(PubsubFixture pubsubFixture)
+    {
+        _pubsubFixture = pubsubFixture;
+        _createTopicWithCloudStorageIngestionSample = new CreateTopicWithCloudStorageIngestionSample();
+    }
+
+    [Fact]
+    public void CreateTopicWithCloudStorageIngestion()
+    {
+        string topicId = _pubsubFixture.RandomTopicId();
+        string bucket = _pubsubFixture.RandomName("Bucket");
+        string inputFormat = "text";
+        string textDelimiter = "\n";
+        string matchGlob = "**.txt";
+        string minimumObjectCreateTime = "1970-01-01T00:00:00Z";
+        Topic createdTopic = _createTopicWithCloudStorageIngestionSample.CreateTopicWithCloudStorageIngestion(_pubsubFixture.ProjectId, topicId, bucket, inputFormat, textDelimiter, matchGlob, minimumObjectCreateTime);
+
+        // confirm that the created topic and topic retrieved by ID are equal
+        Topic retrievedTopic = _pubsubFixture.GetTopic(topicId);
+        Assert.Equal(createdTopic, retrievedTopic);
+
+        // confirm that all Cloud Storage Ingestion params are equal to expected values
+        Assert.Equal(bucket, createdTopic.IngestionDataSourceSettings.CloudStorage.Bucket);
+        Assert.NotNull(createdTopic.IngestionDataSourceSettings.CloudStorage.TextFormat);
+        Assert.Equal(textDelimiter, createdTopic.IngestionDataSourceSettings.CloudStorage.TextFormat.Delimiter);
+        Assert.Equal(matchGlob, createdTopic.IngestionDataSourceSettings.CloudStorage.MatchGlob);
+        Assert.Equal(Timestamp.FromDateTime(DateTime.Parse(minimumObjectCreateTime)), createdTopic.IngestionDataSourceSettings.CloudStorage.MinimumObjectCreateTime);
+    }
+}
