c39bba659a5e7118d6f95afa12a87429ddba362d
Orlando Emiliano Garcia Diaz <garciadiaz@google.com>
2025-05-30 01:45:22 +0000

    feat(translate): add missing region tag and dotnet format

--- translate/api/Translate.Samples/Text.cs
+++ translate/api/Translate.Samples/Text.cs
@@ -1,4 +1,4 @@
-﻿/*
+/*
  * Copyright 2025 Google LLC
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
@@ -50,4 +50,4 @@ public class TranslateTextSample
         return translationResult;
     }
 }
-// [START translate_translate_text]
+// [END translate_translate_text]
