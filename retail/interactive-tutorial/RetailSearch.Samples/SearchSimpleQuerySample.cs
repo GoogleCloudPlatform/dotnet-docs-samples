a777f3ceb3bccd00c1622ebd7bb42613136995f2
rebeccaellis <rebeccaellis@google.com>
2025-06-10 14:10:29 -0500

    Update SearchSimpleQuerySample.cs

--- retail/interactive-tutorial/RetailSearch.Samples/SearchSimpleQuerySample.cs
+++ retail/interactive-tutorial/RetailSearch.Samples/SearchSimpleQuerySample.cs
@@ -20,6 +20,7 @@ using System.Linq;
 // Calls the Retail API to search for products in a catalog using only a search query.
 
 // [START retail_search_simple_query]
+
 /// <summary>
 /// Search simple query sample class.
 /// </summary>
