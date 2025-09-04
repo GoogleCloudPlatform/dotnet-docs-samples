963ed6c9d53370b7942836a0493db0cd58338fbf
mahendra-google <mahendraw@google.com>
2025-09-04 05:07:11 -0700

    samples(storage); add tests for anywhere cache

---
+++ storage/api/Storage.Samples.Tests/StorageControlDisableAnywhereCacheTest.cs
@@ -0,0 +1,42 @@
+// Copyright 2025 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License").
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+using Xunit;
+
+[Collection(nameof(StorageFixture))]
+public class StorageControlDisableAnywhereCacheTest
+{
+    private readonly StorageFixture _fixture;
+
+    public StorageControlDisableAnywhereCacheTest(StorageFixture fixture)
+    {
+        _fixture = fixture;
+    }
+
+    [Fact]
+    public void TestStorageControlDisableAnywhereCache()
+    {
+        StorageControlCreateAnywhereCacheSample createCacheSample = new StorageControlCreateAnywhereCacheSample();
+
+        var bucketName = _fixture.GenerateBucketName();
+        _fixture.CreateBucket(bucketName, "us");
+
+        var createdCache = createCacheSample.StorageControlCreateAnywhereCache(bucketName, "us-west1-a");
+
+        StorageControlDisableAnywhereCacheSample disableCacheSample = new StorageControlDisableAnywhereCacheSample();
+        var disabledCache = disableCacheSample.StorageControlDisableAnywhereCache(bucketName, createdCache.Result.Zone);
+
+        Assert.Equal("disabled", disabledCache.State);
+    }
+}
