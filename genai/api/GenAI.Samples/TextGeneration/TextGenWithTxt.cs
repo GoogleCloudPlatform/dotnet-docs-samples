fa775feaeb7ac98b3495b278a924253fe8242edc
Juan Dominguez <jdomin@softserveinc.com>
2025-11-06 10:54:29 -0300

    feat(genai): add text generation samples (1)

---
+++ genai/api/GenAI.Samples/TextGeneration/TextGenWithTxt.cs
@@ -0,0 +1,43 @@
+/*
+ * Copyright 2025 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+// [START googlegenaisdk_textgen_with_txt]
+
+using Google.GenAI;
+using Google.GenAI.Types;
+using System;
+using System.Threading.Tasks;
+
+public class TextGenWithTxt
+{
+    public async Task<string> GenerateContent(
+        string projectId = "your-project-id",
+        string location = "global",
+        string model = "gemini-2.5-flash")
+    {
+        var client = new Client(project: projectId, location: location, vertexAI: true);
+
+        GenerateContentResponse response = await client.Models.GenerateContentAsync(model: model, contents: "How does AI work?");
+
+        string responseText = response.Candidates[0].Content.Parts[0].Text;
+        Console.WriteLine(responseText);
+        // Example reponse:
+        // AI, or Artificial Intelligence, at its core, is about creating machines that can perform...
+        // Here's a breakdown of how it generally works...
+        return responseText;
+    }
+}
+// [END googlegenaisdk_textgen_with_txt]
