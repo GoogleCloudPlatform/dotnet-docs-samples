4641d0314d01793d9be94c78b1a9cceb89f748a6
khilan maradiya <khilan.maradiya@crestdata.ai>
2026-01-28 17:39:39 +0530

    feat(secretmanager): Reformat code

--- secretmanager/api/SecretManager.Samples/EditSecretLabel.cs
+++ secretmanager/api/SecretManager.Samples/EditSecretLabel.cs
@@ -37,8 +37,6 @@ public class EditSecretLabelSample
         // Get the secret.
         Secret secret = client.GetSecret(name);
 
-        Console.WriteLine($"Updated secret: {secret.Name}");
-
         // Update the labels
         secret.Labels[labelKey] = labelValue;
 
