33098082d18d882347960bc1850c1fb712bf243b
Eric Evans <ericfevans@google.com>
2026-06-05 18:34:30 +0000

    feat(PubSub): Add SMT samples

---
+++ pubsub/api/Pubsub.Samples/CreateSubscriptionWithSingleMessageTransform.cs
@@ -0,0 +1,53 @@
+// Copyright 2026 Google LLC.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// [START pubsub_create_subscription_with_smt]
+
+using Google.Cloud.PubSub.V1;
+using System;
+
+public class CreateSubscriptionWithSingleMessageTransformSample
+{
+    public Subscription CreateSubscriptionWithSingleMessageTransform(string projectId, string topicId, string subscriptionId)
+    {
+        SubscriberServiceApiClient subscriber = SubscriberServiceApiClient.Create();
+        TopicName topicName = TopicName.FromProjectTopic(projectId, topicId);
+
+        SubscriptionName subscriptionName = SubscriptionName.FromProjectSubscription(projectId, subscriptionId);
+        MessageTransform removeSSNTransform = new MessageTransform
+        {
+            JavascriptUdf = new JavaScriptUDF
+            {
+                FunctionName = "redactSsn",
+                Code = "function redactSsn(message, metadata) {"
+                + "   const data = JSON.parse(message.data);"
+                + "   delete data['ssn'];"
+                + "   message.data = JSON.stringify(data);"
+                + "   return message;"
+                + "}"
+            },
+        };
+
+        var subscription = subscriber.CreateSubscription(new Subscription
+        {
+            SubscriptionName = subscriptionName,
+            TopicAsTopicName = topicName,
+            MessageTransforms = { removeSSNTransform },
+        });
+        Console.WriteLine($"Subscription {subscription.Name} created with SMT.");
+
+        return subscription;
+    }
+}
+// [END pubsub_create_subscription_with_smt]
